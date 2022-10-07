c95e17b7b43faf5e2cbaf85c05a8bba3698e4cf0
jaino <msb3302@naver.com>
2022-10-07 14:43:53 +0900

    issue #11 Chore : baseFragment에서 lifeCycleOwner 정의

--- client/app/src/main/java/com/example/witt/presentation/base/BaseFragment.kt
+++ client/app/src/main/java/com/example/witt/presentation/base/BaseFragment.kt
@@ -5,21 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-abstract class BaseFragment<T: ViewDataBinding>(@LayoutRes val layoutRes: Int) : Fragment() {
+abstract class BaseFragment<T: ViewDataBinding>(private val layoutId: Int) : Fragment() {
 
     private var _binding: T? = null
     val binding get() = _binding ?: throw IllegalStateException()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
     }
 
     override fun onDestroyView() {
