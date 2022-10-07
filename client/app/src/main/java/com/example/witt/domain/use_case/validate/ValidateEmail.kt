e4982e7a29b24ed99d719718f225a72be96947b1
jaino <msb3302@naver.com>
2022-10-07 18:13:37 +0900

    issue #12 Chore : email, password, nickname, repeatedPassword Validate useCase 정의하기

---
+++ client/app/src/main/java/com/example/witt/domain/use_case/validate/ValidateEmail.kt
@@ -0,0 +1,24 @@
+package com.example.witt.domain.use_case.validate
+
+import android.util.Patterns
+import com.example.witt.domain.model.ValidationResult
+
+class ValidateEmail {
+    fun execute(email: String): ValidationResult {
+        if(email.isBlank()){
+            return ValidationResult(
+                successful = false,
+                errorMessage = "이메일을 입력해주세요."
+            )
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            return ValidationResult(
+                successful = false,
+                errorMessage = "이메일 형식을 확인해주세요."
+            )
+        }
+        return ValidationResult(
+            successful = true
+        )
+    }
+}
---
+++ client/app/src/main/java/com/example/witt/domain/use_case/validate/ValidateNickName.kt
@@ -0,0 +1,17 @@
+package com.example.witt.domain.use_case.validate
+
+import com.example.witt.domain.model.ValidationResult
+
+class ValidateNickName {
+    fun execute(nickName: String) : ValidationResult {
+        if(nickName.length < 4){
+            return ValidationResult(
+                successful = false,
+                errorMessage = "닉네임을 4글자 이상으로 입력해주세요."
+            )
+        }
+        return ValidationResult(
+            successful = true
+        )
+    }
+}
