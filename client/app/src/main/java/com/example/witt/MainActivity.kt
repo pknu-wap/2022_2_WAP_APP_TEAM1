e196ccb8f5c31311dd706fa69d9bef3d6b72fbf3
jaino <msb3302@naver.com>
2022-10-07 14:43:21 +0900

    issue #11 Chore : Navhost, NavController 정의

--- client/app/src/main/java/com/example/witt/MainActivity.kt
+++ client/app/src/main/java/com/example/witt/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.witt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.login_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
     }
 }
