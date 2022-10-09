20ce5037bfe9f09ea5404611192df8cd27f58eab
jaino <msb3302@naver.com>
2022-10-09 19:01:32 +0900

    issue #12 Feat : validate signUp form

--- client/app/src/main/java/com/example/witt/presentation/ui/signup/SignUpFragment.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/signup/SignUpFragment.kt
@@ -1,11 +1,14 @@
 package com.example.witt.presentation.ui.signup
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.witt.R
 import com.example.witt.databinding.FragmentSignUpBinding
 import com.example.witt.presentation.base.BaseFragment
+import kotlinx.coroutines.launch
 
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up) {
 
@@ -14,10 +17,46 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sig
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //dataBinding viewModel
+        binding.viewModel = viewModel
+
+        initButton()
+        initError()
+        initChannel()
+
+    }
+
+    private fun initButton(){
         binding.goToSignUp2Button.setOnClickListener{
-            viewModel.onEvent(SignUpEvent.SubmitEmailPassword)
-            val direction = SignUpFragmentDirections.actionSignUpFragmentToProfileEditFragment()
-            findNavController().navigate(direction)
+            viewModel.onEvent(SignUpEvent.Submit)
+        }
+    }
+
+    private fun initError(){
+        viewModel.errorEmail.observe(viewLifecycleOwner){ errorMessage ->
+            binding.emailEditTextView.error = errorMessage
+        }
+        viewModel.errorPassword.observe(viewLifecycleOwner){ errorMessage->
+            binding.passwordEditText.error = errorMessage
+        }
+        viewModel.errorRepeatedPassword.observe(viewLifecycleOwner){ errorMessage ->
+            binding.repeatedPasswordEditText.error = errorMessage
+        }
+    }
+
+    private fun initChannel(){
+        lifecycleScope.launch{
+            viewModel.signUpEvents.collect{ event ->
+                when(event){
+                    is SignUpViewModel.SignUpUiEvent.Success -> {
+                        val direction = SignUpFragmentDirections.actionSignUpFragmentToProfileEditFragment()
+                        findNavController().navigate(direction)
+                    }
+                    is SignUpViewModel.SignUpUiEvent.Failure ->{
+                        Toast.makeText(activity, event.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
--- client/app/src/main/java/com/example/witt/presentation/ui/signup/SignUpViewModel.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/signup/SignUpViewModel.kt
@@ -1,29 +1,42 @@
 package com.example.witt.presentation.ui.signup
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.witt.domain.use_case.validate.ValidateEmail
 import com.example.witt.domain.use_case.validate.ValidatePassword
 import com.example.witt.domain.use_case.validate.ValidateRepeatedPassword
+import com.example.witt.presentation.ui.signin.SignInViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 
-class SignUpViewModel(
-    private val validateEmail: ValidateEmail = ValidateEmail(),
-    private val validatePassword: ValidatePassword = ValidatePassword(),
-    private val validateRepeatedPassword: ValidateRepeatedPassword = ValidateRepeatedPassword()
-): ViewModel(){
+class SignUpViewModel: ViewModel(){
+
+    private val validateEmail by lazy { ValidateEmail() }
+    private val validatePassword by lazy { ValidatePassword() }
+    private val validateRepeatedPassword by lazy { ValidateRepeatedPassword() }
+
+    private val signUpEventChannel = Channel<SignUpUiEvent>()
+    val signUpEvents = signUpEventChannel.receiveAsFlow()
 
     //dataBinding 으로 값 가져오기, 더 좋은 방법이 없을까?
-    val inputEmail = MutableLiveData("")
-    val inputPassword = MutableLiveData("")
-    val inputRepeatedPassword = MutableLiveData("")
+    val inputEmail : MutableLiveData<String> = MutableLiveData()
+    val inputPassword : MutableLiveData<String> = MutableLiveData()
+    val inputRepeatedPassword : MutableLiveData<String> = MutableLiveData()
+
+    private val _errorEmail : MutableLiveData<String> = MutableLiveData()
+    val errorEmail : LiveData<String> get() = _errorEmail
+
+    private val _errorPassword : MutableLiveData<String> = MutableLiveData()
+    val errorPassword: LiveData<String> get() = _errorPassword
+
+    private val _errorRepeatedPassword : MutableLiveData<String> = MutableLiveData()
+    val errorRepeatedPassword: LiveData<String> get() = _errorRepeatedPassword
 
     fun onEvent(event: SignUpEvent){
         when(event){
-            is SignUpEvent.SubmitEmailPassword ->{
-
-            }
-            is SignUpEvent.SubmitNamePhoneNumber ->{
-
+            is SignUpEvent.Submit ->{
+                validateData()
             }
         }
     }
@@ -41,13 +54,26 @@ class SignUpViewModel(
             !it.successful
         }
         if(hasError){
+            emailResult.errorMessage?.let { errorMessage ->
+                _errorEmail.value = errorMessage
+            }
+            passwordResult.errorMessage?.let{ errorMessage ->
+                _errorPassword.value = errorMessage
+            }
+            repeatedPasswordResult.errorMessage?.let{ errorMessage ->
+                _errorRepeatedPassword.value = errorMessage
+            }
             return
         }
         submitData()
     }
 
     private fun submitData(){
-        //kakao access token 가져오기 및 데이터 전송
+        signUpEventChannel.trySend(SignUpUiEvent.Success)
     }
 
+    sealed class SignUpUiEvent{
+        data class Failure(val message: String?): SignUpUiEvent()
+        object Success: SignUpUiEvent()
+    }
 }
