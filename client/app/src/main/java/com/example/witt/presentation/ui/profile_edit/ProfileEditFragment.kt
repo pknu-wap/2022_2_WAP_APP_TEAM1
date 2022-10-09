61f39417850cf97ba429033fffb77c268c5326f4
jaino <msb3302@naver.com>
2022-10-09 14:26:22 +0900

    issue #16 Docs : signUp2 -> profile_edit 변경

--- client/app/src/main/java/com/example/witt/presentation/ui/profile_edit/ProfileEditFragment.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/profile_edit/ProfileEditFragment.kt
@@ -1,21 +1,21 @@
-package com.example.witt.presentation.ui.signup
+package com.example.witt.presentation.ui.profile_edit
 
 import android.os.Bundle
 
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.example.witt.R
-import com.example.witt.databinding.FragmentSignUp2Binding
+import com.example.witt.databinding.FragmentProfileEditBinding
 import com.example.witt.presentation.base.BaseFragment
 
 
-class SignUp2Fragment : BaseFragment<FragmentSignUp2Binding>(R.layout.fragment_sign_up2) {
+class ProfileEditFragment : BaseFragment<FragmentProfileEditBinding>(R.layout.fragment_profile_edit) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.signUpButton.setOnClickListener{
-            val direction = SignUp2FragmentDirections.actionSignUp2FragmentToSignInFragment()
+            val direction = ProfileEditFragmentDirections.actionProfileEditFragmentToSignInFragment()
             findNavController().navigate(direction)
         }
     }
