7b2e17c8e89d681f34065eb343e772384ab7f193
jaino <msb3302@naver.com>
2022-10-09 14:52:01 +0900

    issue #26 Chore : HomeFragment 생성

---
+++ client/app/src/main/java/com/example/witt/presentation/ui/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.example.witt.presentation.ui.home
+
+import android.os.Bundle
+import android.view.View
+import com.example.witt.R
+import com.example.witt.databinding.FragmentHomeBinding
+import com.example.witt.presentation.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_profile_edit) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
