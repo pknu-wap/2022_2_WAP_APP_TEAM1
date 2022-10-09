b035196e61c8fca063c5c5c81bb8e8a815298174
jaino <msb3302@naver.com>
2022-10-09 14:25:48 +0900

    issue #16 Docs : error form file 삭제

--- client/app/src/main/java/com/example/witt/presentation/ui/signup/SignUpFromError.kt
+++
@@ -1,7 +0,0 @@
-package com.example.witt.presentation.ui.signup
-
-data class SignUpFromError (
-    val emailError: String = "",
-    val passwordError: String = "",
-    val repeatedPasswordErrro: String = "",
-    )
