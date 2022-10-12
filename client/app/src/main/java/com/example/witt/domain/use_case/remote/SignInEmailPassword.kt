4ff40268cd7494c0d84ae8b62f804785dcd4f42d
jaino <msb3302@naver.com>
2022-10-12 11:26:29 +0900

    issue #28 FEAT : (DOMAIN) 회원가입 repository 명세 및 use_case 정의하기

--- client/app/src/main/java/com/example/witt/domain/use_case/remote/SignInEmailPassword.kt
+++ client/app/src/main/java/com/example/witt/domain/use_case/remote/SignInEmailPassword.kt
@@ -1,6 +1,6 @@
 package com.example.witt.domain.use_case.remote
 
-import com.example.witt.data.auth.response.SignInResponse
+import com.example.witt.domain.model.SignInModel
 import com.example.witt.domain.repository.AuthRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -9,7 +9,7 @@ import javax.inject.Inject
 class SignInEmailPassword @Inject constructor(
     private val authRepository: AuthRepository
 ){
-    suspend operator fun invoke(accountType: Int, email: String, password: String) : Result<SignInResponse> = withContext(Dispatchers.IO){
+    suspend operator fun invoke(accountType: Int, email: String, password: String) : Result<SignInModel> = withContext(Dispatchers.IO){
         authRepository.signIn(accountType, email, password)
     }
 }
