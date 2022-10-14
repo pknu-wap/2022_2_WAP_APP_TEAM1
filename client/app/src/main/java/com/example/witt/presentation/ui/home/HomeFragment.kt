884477870a1e8359e0d6b6b63ca19189d67cfe86
jaino <msb3302@naver.com>
2022-10-14 11:19:26 +0900

    issue #36 Fix : HomeFragment 데이터바인딩 잘못 들어간 부분 수정

--- client/app/src/main/java/com/example/witt/presentation/ui/home/HomeFragment.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/home/HomeFragment.kt
@@ -6,11 +6,10 @@ import com.example.witt.R
 import com.example.witt.databinding.FragmentHomeBinding
 import com.example.witt.presentation.base.BaseFragment
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_profile_edit) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 
 }
