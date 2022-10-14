437f1b7a7332b9c01c8a586ab2967c726911e93f
jaino <msb3302@naver.com>
2022-10-14 11:10:26 +0900

    issue #20 : Feat : (data, domain) data_source 설정하기

---
+++ client/app/src/main/java/com/example/witt/data/source/remote/user/UserTokenDataSourceImpl.kt
@@ -0,0 +1,28 @@
+package com.example.witt.data.source.remote.user
+
+import com.example.witt.data.api.UserTokenService
+import com.example.witt.data.model.user.response.UserTokenResponse
+import com.example.witt.di.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class UserTokenDataSourceImpl @Inject constructor(
+    private val userService: UserTokenService,
+    @IoDispatcher private val coroutineDispatcher: CoroutineDispatcher
+) : UserTokenDataSource{
+    override suspend fun userTokenSignIn(accessToken: String, refreshToken: String): Result<UserTokenResponse> =
+        withContext(coroutineDispatcher){
+        try{
+            val response = userService.tokenSignIn(accessToken)
+            if(response.status){
+            Result.success(response)
+            }else{
+                Result.success(userService.tokenSignIn(refreshToken))
+            }
+        }catch(e: Exception){
+            e.printStackTrace()
+            Result.failure(e)
+        }
+    }
+}
