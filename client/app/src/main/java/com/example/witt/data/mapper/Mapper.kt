73a0331c351dbe2783ba8a2f412ea033a14edab1
jaino <msb3302@naver.com>
2022-10-14 11:12:56 +0900

    issue #20 : Feat : (data -> domain) token_signIn model mapping하기

--- client/app/src/main/java/com/example/witt/data/mapper/Mapper.kt
+++ client/app/src/main/java/com/example/witt/data/mapper/Mapper.kt
@@ -3,9 +3,11 @@ package com.example.witt.data.mapper
 import com.example.witt.data.model.auth.response.SignInResponse
 import com.example.witt.data.model.auth.response.SignUpResponse
 import com.example.witt.data.model.user.response.DuplicateEmailResponse
-import com.example.witt.domain.model.SignInModel
-import com.example.witt.domain.model.SignUpModel
+import com.example.witt.data.model.user.response.UserTokenResponse
+import com.example.witt.domain.model.auth.SignInModel
+import com.example.witt.domain.model.auth.SignUpModel
 import com.example.witt.domain.model.user.DuplicateEmailModel
+import com.example.witt.domain.model.user.UserTokenModel
 
 fun SignUpResponse.toSignUpModel() =  SignUpModel(
     status = status,
@@ -15,8 +17,12 @@ fun SignInResponse.toSignInModel() = SignInModel(
     status = status,
     reason = reason
 )
-fun DuplicateEmailResponse.toDuplicateEmailModel() =
-    DuplicateEmailModel(
+fun DuplicateEmailResponse.toDuplicateEmailModel() = DuplicateEmailModel(
         status = status,
         reason = reason
-    )
+)
+
+fun UserTokenResponse.toUserTokenModel()= UserTokenModel(
+    status = status,
+    reason = reason
+)
