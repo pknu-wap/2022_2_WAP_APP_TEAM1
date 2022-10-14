452bb9fc32f3f857b5603acf93b298d3e4243388
jaino <msb3302@naver.com>
2022-10-14 11:10:51 +0900

    issue #20 : Feat : (data, domain) user Repository에 token_signIn 추가하기

--- client/app/src/main/java/com/example/witt/data/repository/UserRepositoryImpl.kt
+++ client/app/src/main/java/com/example/witt/data/repository/UserRepositoryImpl.kt
@@ -1,18 +1,31 @@
 package com.example.witt.data.repository
 
+import android.content.SharedPreferences
 import com.example.witt.data.model.user.request.DuplicateEmailRequest
 import com.example.witt.data.mapper.toDuplicateEmailModel
+import com.example.witt.data.mapper.toUserTokenModel
 import com.example.witt.data.source.remote.duplicate_check.DuplicateEmailDataSource
+import com.example.witt.data.source.remote.user.UserTokenDataSource
 import com.example.witt.domain.model.user.DuplicateEmailModel
+import com.example.witt.domain.model.user.UserTokenModel
 import com.example.witt.domain.repository.UserRepository
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-    private val dataSource: DuplicateEmailDataSource
-):UserRepository{
+    private val duplicateEmailDataSource: DuplicateEmailDataSource,
+    private val userTokenDataSource: UserTokenDataSource,
+):UserRepository {
     override suspend fun duplicateEmail(email: String): Result<DuplicateEmailModel> {
-        return dataSource.duplicateEmailCheck(DuplicateEmailRequest(email)).mapCatching { response ->
-            response.toDuplicateEmailModel()
-        }
+        return duplicateEmailDataSource.duplicateEmailCheck(DuplicateEmailRequest(email))
+            .mapCatching { response ->
+                response.toDuplicateEmailModel()
+            }
+    }
+
+    override suspend fun userTokenSignIn(accessToken: String, refreshToken: String): Result<UserTokenModel> {
+        return userTokenDataSource.userTokenSignIn(accessToken, refreshToken)
+            .mapCatching { response ->
+                response.toUserTokenModel()
+            }
     }
 }
--- client/app/src/main/java/com/example/witt/domain/repository/UserRepository.kt
+++ client/app/src/main/java/com/example/witt/domain/repository/UserRepository.kt
@@ -1,8 +1,12 @@
 package com.example.witt.domain.repository
 
 import com.example.witt.domain.model.user.DuplicateEmailModel
+import com.example.witt.domain.model.user.UserTokenModel
 
 interface UserRepository {
 
     suspend fun duplicateEmail(email: String): Result<DuplicateEmailModel>
+
+    suspend fun userTokenSignIn(accessToken: String, refreshToken: String): Result<UserTokenModel>
+
 }
