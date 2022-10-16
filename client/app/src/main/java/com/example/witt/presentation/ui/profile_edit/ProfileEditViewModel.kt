8455583edf9fe3a321db80eb5de428bcbc3624f2
jaino <msb3302@naver.com>
2022-10-16 21:01:37 +0900

    issue #29 feat : (presentation) viewModel에서 프로필 파일 저장 후 프로필 업로드 use_case 호출

--- client/app/src/main/java/com/example/witt/presentation/ui/profile_edit/ProfileEditViewModel.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/profile_edit/ProfileEditViewModel.kt
@@ -1,27 +1,31 @@
 package com.example.witt.presentation.ui.profile_edit
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.witt.domain.use_case.local.GetLocalProfile
-import com.example.witt.domain.use_case.local.SetLocalProfile
+import com.example.witt.di.IoDispatcher
+import com.example.witt.domain.use_case.remote.UploadRemoteProfile
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileEditViewModel @Inject constructor(
-    private val getLocalProfile: GetLocalProfile,
-    private val setLocalProfile: SetLocalProfile
+    private val uploadRemoteProfile: UploadRemoteProfile,
+    private val application : Application,
+    @IoDispatcher private val coroutineDispatcher: CoroutineDispatcher
 ): ViewModel() {
 
     val inputName : MutableLiveData<String> = MutableLiveData()
     val inputPhoneNumber : MutableLiveData<String> = MutableLiveData()
-    //수정 필요,, 이미지 양방향 확인해야할듯 하,,
-    val inputImage: MutableLiveData<String> = MutableLiveData()
+
+    //수정 필요,, 이미지 양방향 확인해야할듯, 기본 이미지도 초기화 해줘야할듯
+    private val inputImage: MutableLiveData<String> = MutableLiveData()
 
     private val profileEditChannel = Channel<ProfileEditUiEvent>()
     val profileEditEvents = profileEditChannel.receiveAsFlow()
@@ -31,9 +35,6 @@ class ProfileEditViewModel @Inject constructor(
             is ProfileEditEvent.SubmitProfile ->{
                 submitProfile()
             }
-            is ProfileEditEvent.GetProfile ->{
-                getProfile()
-            }
             is ProfileEditEvent.SubmitProfileImage ->{
                 //자른 이미지 liveData Mapping
                 inputImage.value = event.profileImage
@@ -41,34 +42,38 @@ class ProfileEditViewModel @Inject constructor(
         }
     }
     private fun submitProfile(){
-        viewModelScope.launch {
-            //내부 데이터 베이스 추가 및 네트워크로 보내기 async 활용
-            if(!inputImage.value.isNullOrBlank() && !inputName.value.isNullOrBlank()){
-                val result = setLocalProfile(
-                    inputImage.value ?: "", inputName.value ?: ""
-                ).isSuccess
-                if(result){
-                    profileEditChannel.trySend(ProfileEditUiEvent.SuccessToSetProfile)
+        viewModelScope.launch(coroutineDispatcher) {
+           //todo refactor : 소셜 로그인을 위한 양방향 이미지 처리 구현 및 nullSafety 구현 완벽히
+            if(!inputName.value.isNullOrBlank() && !inputPhoneNumber.value.isNullOrBlank() && !inputImage.value.isNullOrBlank()){
+                val profileFile = File(inputImage.value!!)
+                val file = File(application.cacheDir, "profile.jpg")
+                file.createNewFile()
+                file.outputStream().use {
+                    profileFile.inputStream().copyTo(it)
+                }
+
+                val response = uploadRemoteProfile(file, inputName.value ?: "", inputPhoneNumber.value ?: "")
+
+                response.mapCatching {
+                    if(it.status){
+                        profileEditChannel.trySend(ProfileEditUiEvent.Success)
+                    }else{
+                        profileEditChannel.trySend(ProfileEditUiEvent.Failure(it.reason))
+                    }
+                }.onFailure {
+                    profileEditChannel.trySend(ProfileEditUiEvent.Failure("네트워크를 확인해주세요."))
                 }
             }
-        }
-    }
-    private fun getProfile(){
-        viewModelScope.launch {
-            getLocalProfile().collectLatest { result ->
-                result.mapCatching {
-                    profileEditChannel.trySend(ProfileEditUiEvent.SuccessToGetProfile(
-                        it.profileUri,
-                        it.userName
-                    ))
-                }
+            else {
+                profileEditChannel.trySend(ProfileEditUiEvent.Failure("빈칸을 확인해주세요."))
             }
         }
     }
 
+
+
     sealed class ProfileEditUiEvent{
         data class Failure(val message: String?): ProfileEditUiEvent()
-        data class SuccessToGetProfile(val profileUri: String, val userName: String): ProfileEditUiEvent()
-        object SuccessToSetProfile : ProfileEditUiEvent()
+        object Success: ProfileEditUiEvent()
     }
 }
