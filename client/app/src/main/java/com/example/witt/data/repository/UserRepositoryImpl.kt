542e381b5c75512174dc4cbf289b070cae374771
jaino <msb3302@naver.com>
2022-10-16 20:56:02 +0900

    issue #29 Feat : (data, domain) 프로필 업로드 소스를 활용한 repository 구현

--- client/app/src/main/java/com/example/witt/data/repository/UserRepositoryImpl.kt
+++ client/app/src/main/java/com/example/witt/data/repository/UserRepositoryImpl.kt
@@ -3,24 +3,30 @@ package com.example.witt.data.repository
 import android.content.SharedPreferences
 import com.example.witt.data.model.user.request.DuplicateEmailRequest
 import com.example.witt.data.mapper.toDuplicateEmailModel
+import com.example.witt.data.mapper.toProfileUploadModel
 import com.example.witt.data.mapper.toUserProfileModel
 import com.example.witt.data.mapper.toUserTokenModel
 import com.example.witt.data.model.local.UserProfile
+import com.example.witt.data.model.profile.request.ProfileUploadRequest
 import com.example.witt.data.source.local.user_profile.ProfileDataSource
 import com.example.witt.data.source.remote.duplicate_check.DuplicateEmailDataSource
+import com.example.witt.data.source.remote.profile.ProfileUploadDataSource
 import com.example.witt.data.source.remote.user.UserTokenDataSource
+import com.example.witt.domain.model.profile.remote.ProfileUploadModel
 import com.example.witt.domain.model.user.DuplicateEmailModel
 import com.example.witt.domain.model.user.UserProfileModel
 import com.example.witt.domain.model.user.UserTokenModel
 import com.example.witt.domain.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import java.io.File
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     private val duplicateEmailDataSource: DuplicateEmailDataSource,
     private val userTokenDataSource: UserTokenDataSource,
-    private val profileDataSource: ProfileDataSource
+    private val profileDataSource: ProfileDataSource,
+    private val profileUploadDataSource: ProfileUploadDataSource
 ):UserRepository {
     override suspend fun duplicateEmail(email: String): Result<DuplicateEmailModel> {
         return duplicateEmailDataSource.duplicateEmailCheck(DuplicateEmailRequest(email))
@@ -29,8 +35,8 @@ class UserRepositoryImpl @Inject constructor(
             }
     }
 
-    override suspend fun userTokenSignIn(accessToken: String, refreshToken: String): Result<UserTokenModel> {
-        return userTokenDataSource.userTokenSignIn(accessToken, refreshToken)
+    override suspend fun userTokenSignIn(): Result<UserTokenModel> {
+        return userTokenDataSource.userTokenSignIn()
             .mapCatching { response ->
                 response.toUserTokenModel()
             }
@@ -45,6 +51,22 @@ class UserRepositoryImpl @Inject constructor(
             result.mapCatching { userProfile ->
                 userProfile.toUserProfileModel()
             }
+        }
+    }
+
+    override suspend fun uploadProfile(
+        profile: File,
+        Nickname: String,
+        PhoneNum: String,
+    ): Result<ProfileUploadModel> {
+        return profileUploadDataSource.uploadProfile(
+            profileUploadRequest = ProfileUploadRequest(
+                profile = profile,
+                Nickname = Nickname,
+                PhoneNum = PhoneNum
+            )
+        ).mapCatching { response ->
+            response.toProfileUploadModel()
         }
     }
 }
