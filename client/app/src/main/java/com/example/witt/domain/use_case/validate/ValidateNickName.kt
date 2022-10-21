a8f3a7c808c8e31d7278a24417c1d1f6b1413cfd
jaino <msb3302@naver.com>
2022-10-21 18:59:56 +0900

    issue #50 Chore : (domain) 닉네임 유효성 검사 use_case 작성

--- client/app/src/main/java/com/example/witt/domain/use_case/validate/ValidateNickName.kt
+++ client/app/src/main/java/com/example/witt/domain/use_case/validate/ValidateNickName.kt
@@ -4,10 +4,12 @@ import com.example.witt.domain.model.validate.ValidationResult
 
 class ValidateNickName {
     fun execute(nickName: String) : ValidationResult {
-        if(nickName.length < 4){
+        val containsLettersAndDigits = nickName.any { it.isDigit()} &&
+                nickName.any{ it.isLetter()}
+        if(!containsLettersAndDigits){
             return ValidationResult(
                 successful = false,
-                errorMessage = "닉네임을 4글자 이상으로 입력해주세요."
+                errorMessage = "닉네임은 숫자와 문자로만 구성해주세요."
             )
         }
         return ValidationResult(
