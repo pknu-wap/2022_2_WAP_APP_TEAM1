731fd79fd4f8a1e3c1128aa365cb49dd70cf2d10
jaino <msb3302@naver.com>
2022-10-28 12:52:20 +0900

    issue #29 feature : 갤러리 접근시 permission 설정

--- client/app/src/main/java/com/example/witt/presentation/ui/profile_edit/ProfileEditFragment.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/profile_edit/ProfileEditFragment.kt
@@ -1,5 +1,6 @@
 package com.example.witt.presentation.ui.profile_edit
 
+import android.Manifest
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -14,6 +15,8 @@ import com.canhub.cropper.*
 import com.example.witt.R
 import com.example.witt.databinding.FragmentProfileEditBinding
 import com.example.witt.presentation.base.BaseFragment
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -23,11 +26,23 @@ class ProfileEditFragment : BaseFragment<FragmentProfileEditBinding>(R.layout.fr
     private val viewModel : ProfileEditViewModel by viewModels()
     private val arg: ProfileEditFragmentArgs by navArgs()
 
+    //image cropping
     private val cropImage = registerForActivityResult(CropImageContract()){ result ->
         if(result.isSuccessful){
             uploadProfileImage(imageUri = result.getUriFilePath(requireActivity()))
         }else{
             Toast.makeText(requireActivity(), result.error.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    //permission listener
+    private val permissionListener = object: PermissionListener{
+        override fun onPermissionGranted() {
+            cropCircleImage()
+        }
+
+        override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+            Toast.makeText(requireActivity(), "설정에서 권한을 다시 설정할 수 있습니다.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -42,22 +57,50 @@ class ProfileEditFragment : BaseFragment<FragmentProfileEditBinding>(R.layout.fr
 
     }
 
-    private fun initButton(){
-
+    private fun initButton() {
         binding.profileImage.setOnClickListener {
-            cropImage.launch(
-                options {
-                    setGuidelines(CropImageView.Guidelines.ON)
-                    setAspectRatio(1, 1)
-                    setCropShape(CropImageView.CropShape.OVAL)
-                }
-            )
+            permissionCheck()
         }
 
-        binding.profileEditButton.setOnClickListener{
+        binding.profileEditButton.setOnClickListener {
             viewModel.onEvent(ProfileEditEvent.SubmitProfile)
         }
     }
+
+    private fun permissionCheck() {
+        TedPermission.create()
+            .setPermissionListener(permissionListener)
+            .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .check()
+    }
+
+    private fun cropCircleImage(){
+        cropImage.launch(
+            options {
+                setGuidelines(CropImageView.Guidelines.ON)
+                setAspectRatio(1, 1)
+                setCropShape(CropImageView.CropShape.OVAL)
+            }
+        )
+    }
+
+    //todo arg 처리하기
+    private fun initProfile(){
+        viewModel.inputName.postValue(arg.nickName)
+        uploadProfileImage(arg.profileImage)
+    }
+
+    private fun uploadProfileImage(imageUri: String?){
+        imageUri?.let{
+            Glide.with(requireActivity())
+                .load(imageUri)
+                .placeholder(R.drawable.penguin)
+                .into(binding.profileImage)
+            viewModel.onEvent(ProfileEditEvent.SubmitProfileImage(imageUri))
+        }
+    }
+
+
 
     private fun initChannel(){
         lifecycleScope.launch {
@@ -87,19 +130,4 @@ class ProfileEditFragment : BaseFragment<FragmentProfileEditBinding>(R.layout.fr
         }
     }
 
-    //todo arg 처리하기
-    private fun initProfile(){
-        viewModel.inputName.postValue(arg.nickName)
-        uploadProfileImage(arg.profileImage)
-    }
-
-    private fun uploadProfileImage(imageUri: String?){
-        imageUri?.let{
-            Glide.with(requireActivity())
-                .load(imageUri)
-                .placeholder(R.drawable.penguin)
-                .into(binding.profileImage)
-            viewModel.onEvent(ProfileEditEvent.SubmitProfileImage(imageUri))
-        }
-    }
 }
