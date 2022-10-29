85a466f468a4869727cc376a78baf283f8bb81bf
jaino <msb3302@naver.com>
2022-10-29 13:05:31 +0900

    issue #21 feature : (data, domain) auth repository에 social_signIn 추가하기

--- client/app/src/main/java/com/example/witt/domain/repository/AuthRepository.kt
+++ client/app/src/main/java/com/example/witt/domain/repository/AuthRepository.kt
@@ -2,6 +2,7 @@ package com.example.witt.domain.repository
 
 import com.example.witt.domain.model.auth.SignInModel
 import com.example.witt.domain.model.auth.SignUpModel
+import com.example.witt.domain.model.auth.SocialSignInModel
 import com.example.witt.domain.model.auth.TokenModel
 
 interface AuthRepository {
@@ -11,5 +12,7 @@ interface AuthRepository {
     suspend fun signUp(email: String, password: String): Result<SignUpModel>
 
     suspend fun tokenSignIn(): Result<TokenModel>
+
+    suspend fun socialSignIn(accountType: Int, OauthId: String): Result<SocialSignInModel>
 
 }
