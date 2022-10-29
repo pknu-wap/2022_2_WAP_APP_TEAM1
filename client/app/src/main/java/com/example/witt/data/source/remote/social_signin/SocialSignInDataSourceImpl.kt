04da57ead0c4bf109c1d2da06b336421b332dde1
jaino <msb3302@naver.com>
2022-10-29 13:04:51 +0900

    issue #21 feature : (data) 소셜 로그인 id 서버 전송 data source 작성

---
+++ client/app/src/main/java/com/example/witt/data/source/remote/social_signin/SocialSignInDataSourceImpl.kt
@@ -0,0 +1,24 @@
+package com.example.witt.data.source.remote.social_signin
+
+import com.example.witt.data.api.SocialSignInService
+import com.example.witt.data.model.auth.request.SocialSignInRequest
+import com.example.witt.data.model.auth.response.SocialSignInResponse
+import com.example.witt.di.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class SocialSignInDataSourceImpl @Inject constructor(
+    private val socialSignInService: SocialSignInService,
+     @IoDispatcher private val coroutineDispatcher: CoroutineDispatcher
+) : SocialSignInDataSource{
+    override suspend fun signIn(socialSignInRequest: SocialSignInRequest): Result<SocialSignInResponse> =
+        withContext(coroutineDispatcher){
+            runCatching {
+                val response = socialSignInService.signIn(socialSignInRequest)
+                response
+            }
+        }.onFailure { exception ->
+            exception.cause
+        }
+}
