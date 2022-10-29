2df2f03cd14f6b714cb9be90bf3fb52714f4c3b1
jaino <msb3302@naver.com>
2022-10-29 13:33:47 +0900

    issue #21 feature : 닉네임 유효성 검사 4글자 이상으로 변경

--- client/app/src/main/java/com/example/witt/domain/use_case/validate/ValidateNickName.kt
+++ client/app/src/main/java/com/example/witt/domain/use_case/validate/ValidateNickName.kt
@@ -4,14 +4,13 @@ import com.example.witt.domain.model.validate.ValidationResult
 
 class ValidateNickName {
     fun execute(nickName: String) : ValidationResult {
-        val containsLettersAndDigits = nickName.any { it.isDigit()} &&
-                nickName.any{ it.isLetter()}
-        if(!containsLettersAndDigits){
+        if(nickName.length < 4){
             return ValidationResult(
                 successful = false,
-                errorMessage = "닉네임은 숫자와 문자로만 구성해주세요."
+                errorMessage = "닉네임은 4글자 이상으로 입력해주세요."
             )
         }
+
         return ValidationResult(
             successful = true
         )
