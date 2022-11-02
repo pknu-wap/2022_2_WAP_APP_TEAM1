02d2f2e42c50ff783155b5337b88cb7f162e1242
jaino <msb3302@naver.com>
2022-11-02 20:39:06 +0900

    issue #62 feature : drawUpFragment 기본 세팅

---
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/drawup_plan/DrawUpPlanFragment.kt
@@ -0,0 +1,15 @@
+package com.example.witt.presentation.ui.plan.drawup_plan
+
+import android.os.Bundle
+import android.view.View
+import com.example.witt.R
+import com.example.witt.databinding.FragmentDrawUpPlanBinding
+import com.example.witt.presentation.base.BaseFragment
+
+class DrawUpPlanFragment  : BaseFragment<FragmentDrawUpPlanBinding>(R.layout.fragment_draw_up_plan) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
