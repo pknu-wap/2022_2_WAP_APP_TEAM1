d4e23dfde32da0a2c46887c66f79a92bc13d3854
jaino <msb3302@naver.com>
2022-11-02 10:15:05 +0900

    issue #55 feature : 일정 추가 페이지 View 생성

---
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/make_plan/MakePlanFragment.kt
@@ -0,0 +1,16 @@
+package com.example.witt.presentation.ui.plan.make_plan
+
+import android.os.Bundle
+import android.view.View
+import com.example.witt.R
+import com.example.witt.databinding.FragmentMakePlanBinding
+import com.example.witt.presentation.base.BaseFragment
+
+
+class MakePlanFragment : BaseFragment<FragmentMakePlanBinding>(R.layout.fragment_make_plan) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
