fefe194c0da7a9f64cb6e431be50b5433e5b5ec5
jaino <msb3302@naver.com>
2022-11-03 23:23:57 +0900

    issue #55 feature : onClick event 받아와 현재 목적지 설정 버튼에 매핑

--- client/app/src/main/java/com/example/witt/presentation/ui/plan/make_plan/MakePlanFragment.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/make_plan/MakePlanFragment.kt
@@ -3,21 +3,32 @@ package com.example.witt.presentation.ui.plan.make_plan
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.witt.R
 import com.example.witt.databinding.FragmentMakePlanBinding
 import com.example.witt.presentation.base.BaseFragment
 import com.example.witt.utils.convertTimestampToDate
+import kotlinx.coroutines.launch
 
 
 class MakePlanFragment : BaseFragment<FragmentMakePlanBinding>(R.layout.fragment_make_plan) {
 
     private val viewModel : MakePlanViewModel by viewModels()
-    private val args  by navArgs<MakePlanFragmentArgs>()
+    private val args by navArgs<MakePlanFragmentArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
         initDates()
+        observeData()
+        binding.goToDrawUpPlanButton.setOnClickListener{
+            val direction = MakePlanFragmentDirections.actionMakePlanFragmentToDrawUpPlanFragment()
+            findNavController().navigate(direction)
+        }
     }
 
     private fun initDates(){
@@ -26,8 +37,14 @@ class MakePlanFragment : BaseFragment<FragmentMakePlanBinding>(R.layout.fragment
        viewModel.onEvent(MakePlanEvent.SubmitPlanDate(startTime, endTime))
     }
 
-    private fun initDestinationButton(){
-
+    private fun observeData() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.planDestination.observe(viewLifecycleOwner){
+                    binding.goToDrawUpPlanButton.isEnabled = true
+                    binding.goToDrawUpPlanButton.text = it
+                }
+            }
+        }
     }
-
 }
--- client/app/src/main/java/com/example/witt/presentation/ui/plan/make_plan/MakePlanViewModel.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/make_plan/MakePlanViewModel.kt
@@ -5,11 +5,47 @@ import androidx.lifecycle.ViewModel
 
 class MakePlanViewModel: ViewModel() {
 
+    companion object{
+        const val SEOUL = 1
+        const val BUSAN = 2
+        const val JEJU = 3
+        const val JEONJU = 4
+        const val GANGREUNG = 5
+        const val DAEJEON = 6
+        const val ETC = 7
+    }
+
     private val planStartDate : MutableLiveData<String> = MutableLiveData()
 
     private val planEndDate : MutableLiveData<String> = MutableLiveData()
 
-    private val planDestination : MutableLiveData<String> = MutableLiveData()
+    val planDestination : MutableLiveData<String> = MutableLiveData()
+
+    fun onButtonEvent(destination: Int){
+        when(destination){
+            SEOUL ->{
+                planDestination.value = "서울/경기로 목적지 설정"
+            }
+            BUSAN ->{
+                planDestination.value = "부산/경상으로 목적지 설정"
+            }
+            JEJU ->{
+                planDestination.value = "제주/우도로 목적지 설정"
+            }
+            JEONJU ->{
+                planDestination.value = "전주/전라로 목적지 설정"
+            }
+            GANGREUNG ->{
+                planDestination.value = "강릉/강원으로 목적지 설정"
+            }
+            DAEJEON ->{
+                planDestination.value = "대전/충청으로 목적지 설정"
+            }
+            ETC ->{
+                planDestination.value = "기타 목적지 설정"
+            }
+        }
+    }
 
     fun onEvent(event: MakePlanEvent){
         when(event){
@@ -17,10 +53,6 @@ class MakePlanViewModel: ViewModel() {
                 planStartDate.value = event.startDate
                 planEndDate.value = event.endDate
             }
-            is MakePlanEvent.SubmitDestination ->{
-                planDestination.value = event.destination
-            }
         }
     }
-
 }
