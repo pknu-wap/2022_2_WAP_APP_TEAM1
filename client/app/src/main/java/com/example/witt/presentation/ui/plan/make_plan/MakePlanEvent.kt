6e539b09bf2db8ea7cd1b079f0c7e61af3143c67
jaino <msb3302@naver.com>
2022-11-03 23:24:04 +0900

    issue #55 feature : onClick event 받아와 현재 목적지 설정 버튼에 매핑

--- client/app/src/main/java/com/example/witt/presentation/ui/plan/make_plan/MakePlanEvent.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/make_plan/MakePlanEvent.kt
@@ -3,6 +3,5 @@ package com.example.witt.presentation.ui.plan.make_plan
 sealed class MakePlanEvent {
 
     data class SubmitPlanDate(val startDate: String, val endDate: String): MakePlanEvent()
-    data class SubmitDestination(val destination: String): MakePlanEvent()
 
 }
