7a3389678d86bc14056cba63284dad11e0664772
winocreative <winocreative@gmail.com>
2022-11-04 18:31:50 +0900

    issue #63 chore : OAuthId 제거 및 Username으로 통일화

--- server/src/routers/plan/index.js
+++ server/src/routers/plan/index.js
@@ -1,13 +1,14 @@
-const userRouter = require('express').Router()
-const userController = require('./user.controller')
+const planRouter = require('express').Router()
+const planController = require('./plan.controller')
 const token = require('../../util/jwt')
 const upload = require('../../util/multer')
 
-userRouter.head("/me", token.authenticateRefreshToken, userController.updateToken)
+planController.
+
 userRouter.get("/me", token.authenticateAccessToken, userController.getInfo);
 userRouter.post("/me", userController.login);
 userRouter.patch("/me", upload.single('Image'), token.authenticateAccessToken, userController.editInfo);
-//userRouter.head("/me", );
+userRouter.head("/me", token.authenticateRefreshToken, userController.updateToken);
 
 userRouter.get("/new", userController.duplicate_id);
 userRouter.post("/new", userController.register);
