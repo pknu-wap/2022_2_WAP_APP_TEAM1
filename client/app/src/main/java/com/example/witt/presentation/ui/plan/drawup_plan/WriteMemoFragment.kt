2752bc3fb1befb2e7e2c26d31a865c9ff4f1d731
jaino <msb3302@naver.com>
2022-11-04 11:57:07 +0900

    issue #62 feature : 메모 페이지 작성

---
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/drawup_plan/WriteMemoFragment.kt
@@ -0,0 +1,14 @@
+package com.example.witt.presentation.ui.plan.drawup_plan
+
+import android.os.Bundle
+import android.view.View
+import com.example.witt.R
+import com.example.witt.databinding.FragmentWriteMemoBinding
+import com.example.witt.presentation.base.BaseFragment
+
+class WriteMemoFragment: BaseFragment<FragmentWriteMemoBinding>(R.layout.fragment_write_memo){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
