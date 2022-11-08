47a77924ee5f82c1ea2f83d91f976225c4746bdf
jaino <msb3302@naver.com>
2022-11-08 12:33:41 +0900

    issue #70 feature : default template object 설정

--- client/app/src/main/java/com/example/witt/utils/DefaultTemplate.kt
+++ client/app/src/main/java/com/example/witt/utils/DefaultTemplate.kt
@@ -2,28 +2,23 @@ package com.example.witt.utils
 
 import com.kakao.sdk.template.model.*
 
-object KakaoLinkTemplate {
-    val defaultTemplate by lazy{
+object DefaultTemplate {
+    val kakaoTemplate by lazy{
         FeedTemplate(
             content = Content(
                 title = "WitT의 여행에 초대합니다!",
-                description = "witT_feed",
+                description = "일정을 함께 작성해보세요.",
                 imageUrl = "https://avatars.githubusercontent.com/u/77484719?v=4",
                 link = Link(
                     webUrl = "https://developers.kakao.com",
                     mobileWebUrl = "https://developers.kakao.com"
                 )
             ),
-            social = Social(
-                likeCount = 286,
-                commentCount = 45,
-                sharedCount = 845
-            ),
             buttons = listOf(
                 Button(
-                    "앱으로 보기",
+                    "초대 수락하기",
                     Link(
-                        androidExecutionParams = mapOf("key1" to "value1", "key2" to "value2")
+                        androidExecutionParams = mapOf("key1" to "value1")
                     )
                 )
             )
