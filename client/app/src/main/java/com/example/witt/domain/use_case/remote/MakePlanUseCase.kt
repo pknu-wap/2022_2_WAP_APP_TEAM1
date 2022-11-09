4f14d20c24611ba2f66e97f838e4ad4fb20206dc
jaino <msb3302@naver.com>
2022-11-09 23:34:27 +0900

    issue #55 feature : (domain) 일정 작성 use_case 작성

---
+++ client/app/src/main/java/com/example/witt/domain/use_case/remote/MakePlanUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.witt.domain.use_case.remote
+
+import com.example.witt.domain.model.plan.MakePlanModel
+import com.example.witt.domain.model.plan.MakePlanResponseModel
+import com.example.witt.domain.repository.PlanRepository
+import javax.inject.Inject
+
+class MakePlanUseCase @Inject constructor(
+    private val repository: PlanRepository
+) {
+    suspend operator fun invoke(makePlanModel: MakePlanModel) : Result<MakePlanResponseModel> =
+        repository.makePlan(makePlanModel)
+}
