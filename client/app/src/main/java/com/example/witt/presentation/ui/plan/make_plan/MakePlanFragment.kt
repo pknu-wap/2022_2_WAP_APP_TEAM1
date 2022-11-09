f7d5bb5bd0a7749f5392f62d02148767588ac623
jaino <msb3302@naver.com>
2022-11-09 23:37:36 +0900

    issue #55 feature : (presentation) observe Data 처리하기

--- client/app/src/main/java/com/example/witt/presentation/ui/plan/make_plan/MakePlanFragment.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/make_plan/MakePlanFragment.kt
@@ -1,9 +1,12 @@
 package com.example.witt.presentation.ui.plan.make_plan
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
@@ -12,9 +15,12 @@ import com.example.witt.R
 import com.example.witt.databinding.FragmentMakePlanBinding
 import com.example.witt.presentation.base.BaseFragment
 import com.example.witt.utils.convertTimestampToDate
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class MakePlanFragment : BaseFragment<FragmentMakePlanBinding>(R.layout.fragment_make_plan) {
 
     private val viewModel : MakePlanViewModel by viewModels()
@@ -23,7 +29,7 @@ class MakePlanFragment : BaseFragment<FragmentMakePlanBinding>(R.layout.fragment
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
-        initDates()
+
         observeData()
         initButton()
     }
@@ -34,27 +40,35 @@ class MakePlanFragment : BaseFragment<FragmentMakePlanBinding>(R.layout.fragment
                 binding.planIdTextField.error = "여행 이름을 붙여주세요!"
             }
             else {
-                val direction =
-                    MakePlanFragmentDirections.actionMakePlanFragmentToDrawUpPlanFragment()
-                findNavController().navigate(direction)
+                val startTime = args.startDate.convertTimestampToDate()
+                val endTime = args.endDate.convertTimestampToDate()
+                viewModel.submitPlan(startTime, endTime)
             }
         }
     }
 
-    private fun initDates(){
-        val startTime = args.startDate.convertTimestampToDate()
-        val endTime = args.endDate.convertTimestampToDate()
-       viewModel.onEvent(MakePlanEvent.SubmitPlanDate(startTime, endTime))
-    }
-
+    @SuppressLint("SetTextI18n")
     private fun observeData() {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.planDestination.observe(viewLifecycleOwner){
-                    binding.goToDrawUpPlanButton.isEnabled = true
-                    binding.goToDrawUpPlanButton.text = it
+        //목적지 설정
+        viewModel.planDestination
+            .flowWithLifecycle(viewLifecycleOwner.lifecycle).onEach{
+                binding.goToDrawUpPlanButton.isEnabled = true
+                binding.goToDrawUpPlanButton.text = it + "으로 목적지 설정"
+            }.launchIn(viewLifecycleOwner.lifecycleScope)
+
+        //일정 계획
+        viewModel.makePlanEvent
+            .flowWithLifecycle(viewLifecycleOwner.lifecycle).onEach{ event ->
+                when(event){
+                    is MakePlanViewModel.UiEvent.Success ->{
+                        val direction =
+                            MakePlanFragmentDirections.actionMakePlanFragmentToDrawUpPlanFragment()
+                        findNavController().navigate(direction)
+                    }
+                    is MakePlanViewModel.UiEvent.Failure ->{
+                        Toast.makeText(activity, event.message, Toast.LENGTH_SHORT).show()
+                    }
                 }
-            }
-        }
+            }.launchIn(viewLifecycleOwner.lifecycleScope)
     }
 }
