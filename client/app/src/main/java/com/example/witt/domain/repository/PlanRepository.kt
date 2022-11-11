e6344de8dc607bd4ac6ee76579acd2890924d257
jaino <msb3302@naver.com>
2022-11-11 14:49:11 +0900

    `issue #75 feature: (data, domain) plan Repository 에 GetPlanDataSource를 활용한 메소드 구현

--- client/app/src/main/java/com/example/witt/domain/repository/PlanRepository.kt
+++ client/app/src/main/java/com/example/witt/domain/repository/PlanRepository.kt
@@ -1,10 +1,14 @@
 package com.example.witt.domain.repository
 
+import com.example.witt.domain.model.plan.GetPlanModel
 import com.example.witt.domain.model.plan.MakePlanModel
 import com.example.witt.domain.model.plan.MakePlanResponseModel
+import kotlinx.coroutines.flow.Flow
 
 interface PlanRepository {
 
     suspend fun makePlan(makePlanModel: MakePlanModel): Result<MakePlanResponseModel>
+
+    suspend fun getPlan(): Result<List<GetPlanModel>>
 
 }
