eecd9012f9870f6976d61e37c91b17de890de6b0
jaino <msb3302@naver.com>
2022-11-11 14:55:46 +0900

    `issue #75 feature: recyclerView에 어댑터 연결 및 viewModel data observing

--- client/app/src/main/java/com/example/witt/presentation/ui/home/HomeFragment.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/home/HomeFragment.kt
@@ -2,13 +2,23 @@ package com.example.witt.presentation.ui.home
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.core.util.Pair
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.witt.R
 import com.example.witt.databinding.FragmentHomeBinding
 import com.example.witt.presentation.base.BaseFragment
+import com.example.witt.presentation.ui.home.adapter.HomePlanAdapter
 import com.google.android.material.datepicker.MaterialDatePicker
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
+@AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     private val dateRangePicker by lazy {
@@ -24,10 +34,51 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
             .build()
     }
 
+    private val viewModel: HomeViewModel by viewModels()
+
+    private lateinit var homePlanAdapter: HomePlanAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        observeData()
+        initAdapter()
         initButton()
         initDateRangePicker()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getPlanList()
+    }
+
+    private fun observeData(){
+        viewModel.homeEvent.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach {
+                when(it){
+                    is HomeViewModel.UiEvent.Success ->{}
+                    is HomeViewModel.UiEvent.Failure ->{
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }.launchIn(viewLifecycleOwner.lifecycleScope)
+
+        viewModel.planList.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach {
+                homePlanAdapter.submitList(it)
+            }.launchIn(viewLifecycleOwner.lifecycleScope)
+
+    }
+
+    private fun initAdapter(){
+        homePlanAdapter = HomePlanAdapter(
+            onPlanCardClick = {
+                //todo drawUpPlanFragment navigate
+            }
+        )
+        homePlanAdapter.submitList(emptyList())
+        binding.homePlanRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.homePlanRecyclerView.adapter = homePlanAdapter
     }
 
     private fun initButton(){
@@ -35,6 +86,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
             dateRangePicker.show(requireActivity().supportFragmentManager, "datePicker")
         }
     }
+
     private fun initDateRangePicker() {
         //백스택 후 재사용시 navigate 오류 발생
         dateRangePicker.addOnPositiveButtonClickListener {
