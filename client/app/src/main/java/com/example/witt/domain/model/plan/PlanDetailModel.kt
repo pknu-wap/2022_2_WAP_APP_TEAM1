e10f87ef1f3bea3c1256eb6cd11e31f04eadb273
jaino <msb3302@naver.com>
2022-11-12 14:14:45 +0900

    `issue #62 feature : PlanDetailModel 구현

---
+++ client/app/src/main/java/com/example/witt/domain/model/plan/PlanDetailModel.kt
@@ -0,0 +1,10 @@
+package com.example.witt.domain.model.plan
+
+
+data class PlanDetailModel (
+    val PlanId: String,
+    val StartDate: String,
+    val EndDate: String,
+    val Name: String,
+    val Region: String
+)
