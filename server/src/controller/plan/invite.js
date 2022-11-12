cc5ef1fb5c578386ad82a18da30ae8750089662b
winocreative <winocreative@gmail.com>
2022-11-12 23:31:32 +0900

    issue #63 chore : Promise에서 await async로 변경

--- server/src/controller/plan/invite.js
+++ server/src/controller/plan/invite.js
@@ -5,97 +5,69 @@ const { raw2str } = require("../../util/rawtostr")
 
 module.exports = {
     //Join Plan(PUT)
-    joinPlan(req, res) {
+    async joinPlan(req, res) {
         if (req.body.PlanId === undefined) {
             return res.status(400).send({
                 message: "Bad Request"
             });
         }
-        models.Plan.isMemberOf(models, req.body.PlanId, req.token.UserId).then(isMember => {
-            if (isMember) {
-                return res.status(403).send({
-                    message: "Forbidden"
-                });
-            }
-        }).catch(err => {
-            return res.status(500).send({
-                message: err.message
+        let isMember = await models.Plan.isMemberOf(models, req.body.PlanId, req.token.UserId)
+
+        if (isMember) {
+            return res.status(403).send({
+                message: "Forbidden"
             });
-        });
-        models.Plan.findOne({
+        }
+        let plan = await models.Plan.findOne({
             where: {
                 PlanId: req.body.PlanId
             }
-        }).then(plan => {
-            if (plan === null) {
-                return res.status(404).send({
-                    message: "Not Found"
-                });
-            }
-            models.PlanParticipant.create({
-                PlanId: req.body.PlanId,
-                UserId: req.token.UserId
-            }).then(planParticipant => {
-                return res.status(200).send({
-                    message: { status: true, reason: "일정 참여 성공" },
-                });
-            }).catch(err => {
-                return res.status(500).send({
-                    message: err.message
-                });
+        });
+        if (plan === null) {
+            return res.status(404).send({
+                message: "Not Found"
             });
-        }).catch(err => {
-            return res.status(500).send({
-                message: err.message
-            });
+        }
+        let planParticipant = await models.PlanParticipant.create({
+            PlanId: req.body.PlanId,
+            UserId: req.token.UserId
+        });
+        return res.status(200).send({
+            message: { status: true, reason: "일정 참여 성공" },
         });
     },
     //Leave Plan(DELETE)
-    leavePlan(req, res) {
+    async leavePlan(req, res) {
         if (req.body.PlanId === undefined) {
             return res.status(400).send({
                 message: "Bad Request"
             });
         }
-        models.Plan.isMemberOf(models, req.body.PlanId, req.token.UserId).then(isMember => {
-            if (!isMember) {
-                return res.status(403).send({
-                    message: "Forbidden"
-                });
-            }
-        }).catch(err => {
-            return res.status(500).send({
-                message: err.message
+        let isMember = await models.Plan.isMemberOf(models, req.body.PlanId, req.token.UserId)
+        if (!isMember) {
+            return res.status(403).send({
+                message: "Forbidden"
             });
-        });
-        models.Plan.findOne({
+        }
+        let plan = await models.Plan.findOne({
             where: {
                 PlanId: req.body.PlanId
             }
-        }).then(plan => {
-            if (plan === null) {
-                return res.status(404).send({
-                    message: "Not Found"
-                });
-            }
-            models.PlanParticipant.destroy({
-                where: {
-                    PlanId: req.body.PlanId,
-                    UserId: req.token.UserId
-                }
-            }).then(planParticipant => {
-                return res.status(200).send({
-                    message: { status: true, reason: "일정 탈퇴 성공" },
-                });
-            }).catch(err => {
-                return res.status(500).send({
-                    message: err.message
-                });
-            });
-        }).catch(err => {
-            return res.status(500).send({
-                message: err.message
-            });
         });
-    }
+        if (plan === null) {
+            return res.status(404).send({
+                message: "Not Found"
+            });
+        }
+        let result = await models.PlanParticipant.destroy({
+            where: {
+                PlanId: req.body.PlanId,
+                UserId: req.token.UserId
+            }
+        })
+
+        return res.status(200).send({
+            message: { status: true, reason: "일정 탈퇴 성공" },
+        });
+    },
 }
