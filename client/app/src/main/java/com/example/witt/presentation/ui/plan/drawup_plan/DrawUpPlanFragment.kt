0ee9501970940d8c92634f0475aa0f2d1a7b67b7
jaino <msb3302@naver.com>
2022-11-14 10:49:44 +0900

    issue #62 feature : planViewModel dataBinding

--- client/app/src/main/java/com/example/witt/presentation/ui/plan/drawup_plan/DrawUpPlanFragment.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/drawup_plan/DrawUpPlanFragment.kt
@@ -5,11 +5,13 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.witt.R
 import com.example.witt.databinding.FragmentDrawUpPlanBinding
 import com.example.witt.presentation.base.BaseFragment
+import com.example.witt.presentation.ui.plan.PlanViewModel
 import com.example.witt.presentation.ui.plan.drawup_plan.adapter.DatePlanAdapter
 import com.example.witt.presentation.ui.plan.drawup_plan.adapter.TimePlanAdapter
 import com.example.witt.presentation.ui.plan.drawup_plan.example.PlanDummy
@@ -28,20 +30,15 @@ class DrawUpPlanFragment  : BaseFragment<FragmentDrawUpPlanBinding>(R.layout.fra
 
     private lateinit var timePlanAdapter: TimePlanAdapter
     private lateinit var datePlanAdapter: DatePlanAdapter
+    private val viewModel by activityViewModels<PlanViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
 
-        initMap()
+        //initMap()
         initAdapter()
-        initView()
         initButton()
-    }
-
-    private fun initView() {
-        binding.dateTextView.text = "2022.11.21 ~ 2022.11.23"
-        binding.destinationTextView.text = "부산/경상"
-        binding.planNameTextView.text = "성훈쿤의 생일기념 여행"
     }
 
     private fun initButton() {
@@ -52,14 +49,8 @@ class DrawUpPlanFragment  : BaseFragment<FragmentDrawUpPlanBinding>(R.layout.fra
 
     private fun initAdapter() {
         //시간순 어댑터
-        timePlanAdapter = TimePlanAdapter(
-            memoClick = {
-                showMemoDialog(it.memo)
-            }
-        )
-
+        timePlanAdapter = TimePlanAdapter(memoClick = { showMemoDialog(it.memo) })
         timePlanAdapter.submitList(PlanDummy.getTimePlan())
-
 
         //날짜 어댑터
         datePlanAdapter = DatePlanAdapter(
@@ -95,7 +86,6 @@ class DrawUpPlanFragment  : BaseFragment<FragmentDrawUpPlanBinding>(R.layout.fra
             MapPOIItem.MarkerType.RedPin // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
         mapView.addPOIItem(marker)
 
-
     }
 
     private fun showMemoDialog(memo: String?) {
@@ -110,31 +100,23 @@ class DrawUpPlanFragment  : BaseFragment<FragmentDrawUpPlanBinding>(R.layout.fra
 
     private fun sendKakaoLink(context: Context, defaultFeed: FeedTemplate) {
         if (ShareClient.instance.isKakaoTalkSharingAvailable(context)) {
-            // 카카오톡으로 카카오톡 공유 가능
             ShareClient.instance.shareDefault(context, defaultFeed) { sharingResult, error ->
                 if (sharingResult != null) {
                     Log.d("tag", "카카오톡 공유 성공 ${sharingResult.intent}")
                     startActivity(sharingResult.intent)
-
-                    // 카카오톡 공유에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
-                    Log.w("tag", "Warning Msg: ${sharingResult.warningMsg}")
-                    Log.w("tag", "Argument Msg: ${sharingResult.argumentMsg}")
                 }
             }
-        } else {
-            // 웹 공유 예시 코드
+        } else { // 웹 공유 예시 코드
             val sharerUrl = WebSharerClient.instance.makeDefaultUrl(defaultFeed)
-
             try {
                 KakaoCustomTabsClient.openWithDefault(context, sharerUrl)
             } catch (e: UnsupportedOperationException) {
-                //todo CustomTabsServiceConnection 지원 브라우저가 없을 때 예외처리
+                e.printStackTrace()
             }
-
             try {
                 KakaoCustomTabsClient.open(context, sharerUrl)
             } catch (e: ActivityNotFoundException) {
-                //todo 디바이스에 설치된 인터넷 브라우저가 없을 때 예외처리
+                e.printStackTrace()
             }
         }
     }
