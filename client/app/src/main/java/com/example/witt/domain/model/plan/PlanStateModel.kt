5e5199ab53551984103cc42def652b7ac75a3b47
jaino <msb3302@naver.com>
2022-11-14 10:45:58 +0900

    issue #62 rename : PlanDetailModel -> PlanStateModel

--- client/app/src/main/java/com/example/witt/domain/model/plan/PlanStateModel.kt
+++ client/app/src/main/java/com/example/witt/domain/model/plan/PlanStateModel.kt
@@ -1,7 +1,7 @@
 package com.example.witt.domain.model.plan
 
 
-data class PlanDetailModel (
+data class PlanStateModel (
     val PlanId: String,
     val StartDate: String,
     val EndDate: String,
--- client/app/src/main/java/com/example/witt/presentation/ui/plan/PlanViewModel.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/PlanViewModel.kt
@@ -1,25 +1,21 @@
 package com.example.witt.presentation.ui.plan
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.witt.domain.model.plan.PlanDetailModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import com.example.witt.domain.model.plan.PlanStateModel
 import kotlinx.coroutines.launch
 
 class PlanViewModel : ViewModel(){
 
-    private var _planDetail :MutableStateFlow<PlanDetailModel> = MutableStateFlow(PlanDetailModel("","","","",""))
-    val planDetail : StateFlow<PlanDetailModel> get() = _planDetail
+    //dataBinding
+    private var _planState : MutableLiveData<PlanStateModel> = MutableLiveData()
+    val planState : LiveData<PlanStateModel> get() = _planState
 
-    //get plan
-    // 1번 만들어진 일정에서 클릭
-
-    // 2번 일정을 만들 때
-
-    fun setPlanDetail(planModel: PlanDetailModel){
+    fun setPlanState(planItem: PlanStateModel){
         viewModelScope.launch {
-            _planDetail.value = planModel
+            _planState.value = planItem
         }
     }
 }
