3ddfcc2ed823f488692dd0010da03ed1e7ad443c
winocreative <winocreative@gmail.com>
2022-11-15 13:21:18 +0900

    issue #63 chore : Place 관련 API 수정

--- server/src/routers/user/index.js
+++ server/src/routers/user/index.js
@@ -2,13 +2,21 @@ const userRouter = require('express').Router()
 const userService = require('../../controller/user')
 const token = require('../../util/jwt')
 const multer = require('multer')
-
-userRouter.head("/me", token.authenticateRefreshToken, userService.updateToken)
-userRouter.get("/me", token.authenticateAccessToken, userService.getInfo);
-userRouter.post("/me", userService.login);
-userRouter.patch("/me", [multer().single('Image'), token.authenticateAccessToken], userService.updateInfo);
-
-userRouter.get("/new", userService.duplicateCheck);
-userRouter.post("/new", userService.register);
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(next);
+    };
+}
+function multipartImage(req, res, next) {
+        multer().single('Image')(req, res, function (err) {
+            return res.status(400).send("Invalid image");
+        });
+}
+userRouter.head("/me", token.authenticateRefreshToken, wrapAsync(userService.updateToken));
+userRouter.get("/me", token.authenticateAccessToken, wrapAsync(userService.getInfo));
+userRouter.post("/me", wrapAsync(userService.login));
+userRouter.patch("/me", [multipartImage, token.authenticateAccessToken], wrapAsync(userService.updateInfo));
+userRouter.get("/new", wrapAsync(userService.duplicateCheck));
+userRouter.post("/new", wrapAsync(userService.register));
 
 module.exports = userRouter;
