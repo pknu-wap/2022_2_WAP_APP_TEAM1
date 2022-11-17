4226b0badce1a00f253eaad5a17322f3faf31176
jaino <msb3302@naver.com>
2022-11-17 22:14:35 +0900

    issue #62 feature : 날짜 UseCase Default Dispatcher 사용 및 메모 추가

--- client/app/src/main/java/com/example/witt/domain/use_case/date/GetDateDayUseCase.kt
+++ client/app/src/main/java/com/example/witt/domain/use_case/date/GetDateDayUseCase.kt
@@ -1,13 +1,19 @@
 package com.example.witt.domain.use_case.date
 
+import com.example.witt.di.DefaultDispatcher
 import com.example.witt.domain.model.plan.DayPlanModel
+import com.example.witt.domain.model.plan.get_plan.DetailPlanModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import javax.inject.Inject
 
-class GetDateDayUseCase {
-
-    operator fun invoke(startDate: String, endDate: String) : List<DayPlanModel>{
-
+class GetDateDayUseCase @Inject constructor(
+    @DefaultDispatcher private val coroutineDispatcher: CoroutineDispatcher
+){
+     suspend operator fun invoke(startDate: String, endDate: String, data: List<DetailPlanModel>) : List<DayPlanModel>
+     = withContext(coroutineDispatcher){
         var sd = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyy.MM.dd"))
         val ed = LocalDate.parse(endDate, DateTimeFormatter.ofPattern("yyyy.MM.dd"))
 
@@ -15,11 +21,10 @@ class GetDateDayUseCase {
 
         var i = 1
         while(sd <= ed) {
-            result.add(DayPlanModel("day $i", sd.toString()))
+            result.add(DayPlanModel("Day $i", sd.toString(), data.filter{it.Day == i}))
             sd = sd.plusDays(1)
             i += 1
         }
-
-        return result
+         result
     }
 }
