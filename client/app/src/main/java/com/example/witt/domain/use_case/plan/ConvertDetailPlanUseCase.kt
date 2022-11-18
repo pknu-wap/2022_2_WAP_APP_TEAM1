6be5b78080ae6417c387eead6779a164c794f06c
jaino <msb3302@naver.com>
2022-11-18 09:50:29 +0900

    issue #62 rename : GetDayDateUseCase -> ConvertDetailPlanUseCase

--- client/app/src/main/java/com/example/witt/domain/use_case/plan/ConvertDetailPlanUseCase.kt
+++ client/app/src/main/java/com/example/witt/domain/use_case/plan/ConvertDetailPlanUseCase.kt
@@ -1,7 +1,7 @@
-package com.example.witt.domain.use_case.date
+package com.example.witt.domain.use_case.plan
 
 import com.example.witt.di.DefaultDispatcher
-import com.example.witt.domain.model.plan.DayPlanModel
+import com.example.witt.domain.model.plan.adapter.PlanModel
 import com.example.witt.domain.model.plan.get_plan.DetailPlanModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
@@ -9,19 +9,19 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
-class GetDateDayUseCase @Inject constructor(
+class ConvertDetailPlanUseCase @Inject constructor(
     @DefaultDispatcher private val coroutineDispatcher: CoroutineDispatcher
 ){
-     suspend operator fun invoke(startDate: String, endDate: String, data: List<DetailPlanModel>) : List<DayPlanModel>
+     suspend operator fun invoke(startDate: String, endDate: String, data: List<DetailPlanModel>) : List<PlanModel>
      = withContext(coroutineDispatcher){
         var sd = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyy.MM.dd"))
         val ed = LocalDate.parse(endDate, DateTimeFormatter.ofPattern("yyyy.MM.dd"))
 
-        val result : MutableList<DayPlanModel> = mutableListOf()
+        val result : MutableList<PlanModel> = mutableListOf()
 
         var i = 1
         while(sd <= ed) {
-            result.add(DayPlanModel("Day $i", sd.toString(), data.filter{it.Day == i}))
+            result.add(PlanModel("Day $i", sd.toString(), data.filter{it.Day == i}))
             sd = sd.plusDays(1)
             i += 1
         }
