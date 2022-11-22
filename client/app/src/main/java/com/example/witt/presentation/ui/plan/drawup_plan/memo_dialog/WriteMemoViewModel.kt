1075c35c1716ede7312927b4162efbf09cab0e1d
jaino <msb3302@naver.com>
2022-11-22 14:47:32 +0900

    issue #78 feature : addMemo, EditMemo 함수 작성 및 호출

--- client/app/src/main/java/com/example/witt/presentation/ui/plan/drawup_plan/memo_dialog/WriteMemoViewModel.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/drawup_plan/memo_dialog/WriteMemoViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.witt.presentation.ui.plan.drawup_plan.memo_dialog
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -24,24 +23,59 @@ class WriteMemoViewModel @Inject constructor(
     val writeMemoEvent: SharedFlow<UiEvent<Unit>> get() = _writeMemoEvent
 
     private val tripId : MutableLiveData<Int> = MutableLiveData()
+
+    private val planId : MutableLiveData<Int> = MutableLiveData()
+
     private val dayId : MutableLiveData<Int> = MutableLiveData()
 
     fun setTripId(memoTripId: Int){
         tripId.value = memoTripId
     }
 
-    fun setMemoInfo(memo: String, memoDay: Int){
+    fun setMemoInfo(memo: String, memoDay: Int?, memoPlanId: Int?){
         inputMemo.value = memo
-        dayId.value = memoDay
+        memoDay?.let { dayId.value = it}
+        memoPlanId?.let { planId.value = it }
     }
 
     fun submitMemo(){
+        //planId를 가지면 edit, 아니면 add
+        if(planId.value != -1){
+            editMemo()
+        }else{
+            addMemo()
+        }
+    }
+
+    private fun addMemo(){
         viewModelScope.launch {
             if(!inputMemo.value.isNullOrEmpty()){
                 planRepository.makeMemo(
                     tripId = requireNotNull(tripId.value),
                     day = requireNotNull(dayId.value),
                     content = requireNotNull(inputMemo.value)
+                ).mapCatching {
+                    if(it.status){
+                        _writeMemoEvent.emit(UiEvent.Success(Unit))
+                    }else{
+                        _writeMemoEvent.emit(UiEvent.Failure(it.reason))
+                    }
+                }.onFailure {
+                    _writeMemoEvent.emit(UiEvent.Failure("네트워크를 확인해주세요."))
+                }
+            }else{
+                _writeMemoEvent.emit(UiEvent.Failure("내용을 확인해주세요."))
+            }
+        }
+    }
+
+    private fun editMemo(){
+        viewModelScope.launch {
+            if(!inputMemo.value.isNullOrEmpty()){
+                planRepository.editMemo(
+                    tripId = requireNotNull(tripId.value),
+                    Content = requireNotNull(inputMemo.value),
+                    planId = requireNotNull(planId.value)
                 ).mapCatching {
                     if(it.status){
                         _writeMemoEvent.emit(UiEvent.Success(Unit))
