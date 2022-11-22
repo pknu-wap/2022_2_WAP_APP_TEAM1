f5f9b8c6d5f044a9c4627e63bac5fdb092cab37a
jaino <msb3302@naver.com>
2022-11-22 20:20:20 +0900

    issue #87 feature : RemovePlanDataSource 의존성 주입 to detailPlanRepository

--- client/app/src/main/java/com/example/witt/di/RepositoryModule.kt
+++ client/app/src/main/java/com/example/witt/di/RepositoryModule.kt
@@ -2,6 +2,7 @@ package com.example.witt.di
 
 import android.content.SharedPreferences
 import com.example.witt.data.repository.AuthRepositoryImpl
+import com.example.witt.data.repository.DetailPlanRepositoryImpl
 import com.example.witt.data.repository.PlanRepositoryImpl
 import com.example.witt.data.repository.UserRepositoryImpl
 import com.example.witt.data.source.local.user_profile.ProfileDataSource
@@ -11,12 +12,14 @@ import com.example.witt.data.source.remote.memo.MakeMemoDataSource
 import com.example.witt.data.source.remote.plan.get_plan.GetPlanDataSource
 import com.example.witt.data.source.remote.plan.get_plan.GetPlanListDataSource
 import com.example.witt.data.source.remote.plan.make_plan.MakePlanDataSource
+import com.example.witt.data.source.remote.plan.remove_plan.RemovePlanDataSource
 import com.example.witt.data.source.remote.profile.ProfileUploadDataSource
 import com.example.witt.data.source.remote.signin.SignInDataSource
 import com.example.witt.data.source.remote.signup.SignUpDataSource
 import com.example.witt.data.source.remote.social_signin.SocialSignInDataSource
 import com.example.witt.data.source.remote.token_signin.TokenSignInDataSource
 import com.example.witt.domain.repository.AuthRepository
+import com.example.witt.domain.repository.DetailPlanRepository
 import com.example.witt.domain.repository.PlanRepository
 import com.example.witt.domain.repository.UserRepository
 import dagger.Module
@@ -54,11 +57,18 @@ object RepositoryModule {
     fun providePlanRepository(makePlanDataSource: MakePlanDataSource,
                               getPlanListDataSource: GetPlanListDataSource,
                               getPlanDataSource: GetPlanDataSource,
-                              makeMemoDataSource: MakeMemoDataSource,
-                              editMemoDataSource: EditMemoDataSource
+                              removePlanDataSource: RemovePlanDataSource
     ): PlanRepository{
         return PlanRepositoryImpl(makePlanDataSource, getPlanListDataSource,
-            getPlanDataSource, makeMemoDataSource, editMemoDataSource)
+            getPlanDataSource, removePlanDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDetailPlanRepository(makeMemoDataSource: MakeMemoDataSource,
+                                    editMemoDataSource: EditMemoDataSource
+    ): DetailPlanRepository{
+        return DetailPlanRepositoryImpl(makeMemoDataSource, editMemoDataSource)
     }
 
 }
