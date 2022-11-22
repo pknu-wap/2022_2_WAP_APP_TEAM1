bcb196e03180285a6e761642f9459ce089385c58
jaino <msb3302@naver.com>
2022-11-22 20:11:31 +0900

    issue #87 feature : remove Plan API interface 작성

--- client/app/src/main/java/com/example/witt/data/api/PlanService.kt
+++ client/app/src/main/java/com/example/witt/data/api/PlanService.kt
@@ -2,14 +2,21 @@ package com.example.witt.data.api
 
 import com.example.witt.data.model.plan.make_plan.request.MakePlanRequest
 import com.example.witt.data.model.plan.make_plan.response.MakePlanResponse
+import com.example.witt.data.model.plan.remove_plan.RemovePlanResponse
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.PUT
 
-interface MakePlanService {
+interface PlanService {
 
     @PUT("/api/trip")
     suspend fun makePlan(
         @Body request: MakePlanRequest
     ):MakePlanResponse
+
+    @DELETE("/api/trip")
+    suspend fun removePlan(
+        @Body tripId: Int
+    ): RemovePlanResponse
 
 }
