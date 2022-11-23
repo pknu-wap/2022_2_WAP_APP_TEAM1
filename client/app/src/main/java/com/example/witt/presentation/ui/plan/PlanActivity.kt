497050e3531e6d82987e299e58f2446ea89fe461
jaino <msb3302@naver.com>
2022-11-23 10:22:15 +0900

    issue #88 feature : 초대 받은 경우 dialog 생성 및 값 처리

--- client/app/src/main/java/com/example/witt/presentation/ui/plan/PlanActivity.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/PlanActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.witt.R
+import com.example.witt.presentation.widget.JoinPlanDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,5 +23,37 @@ class PlanActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.plan_nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
+        checkJoinPlan()
     }
+
+    private fun checkJoinPlan(){
+        val prefs = getSharedPreferences("prefs", MODE_PRIVATE)
+        if(!prefs.getString("tripId", null).isNullOrBlank()){
+            val tripName = prefs.getString("tripName", null) ?: return
+            val tripDate = prefs.getString("tripDate", null) ?: return
+            showJoinPlanDialog(tripName, tripDate)
+        }
+    }
+
+    private fun showJoinPlanDialog(tripName: String, tripDate: String){
+        JoinPlanDialog(
+            this, tripName, tripDate,
+            onClickCancel = {
+                rejectPlan()
+            },
+            onClickJoin = {
+                viewModel.joinPlan()
+            }
+        ).show()
+    }
+
+    //sharedPreference에 값 삭제
+    private fun rejectPlan(){
+        getSharedPreferences("prefs", MODE_PRIVATE).edit()
+            .remove("tripId")
+            .remove("tripName")
+            .remove("tripDate")
+            .apply()
+    }
+
 }
