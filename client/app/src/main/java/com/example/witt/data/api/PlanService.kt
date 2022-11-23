074b69fd97adc513a223d2579f36f401014acda4
jaino <msb3302@naver.com>
2022-11-23 11:24:43 +0900

    issue #88 feature : create planJoin api interface

--- client/app/src/main/java/com/example/witt/data/api/PlanService.kt
+++ client/app/src/main/java/com/example/witt/data/api/PlanService.kt
@@ -2,6 +2,7 @@ package com.example.witt.data.api
 
 import com.example.witt.data.model.plan.make_plan.request.MakePlanRequest
 import com.example.witt.data.model.plan.make_plan.response.MakePlanResponse
+import com.example.witt.data.model.plan.remove_plan.JoinPlanResponse
 import com.example.witt.data.model.plan.remove_plan.RemovePlanResponse
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -18,5 +19,10 @@ interface PlanService {
     suspend fun removePlan(
         @Body tripId: Int
     ): RemovePlanResponse
+
+    @PUT("/api/trip")
+    suspend fun joinPlan(
+        @Body tripId: Int
+    ): JoinPlanResponse
 
 }
