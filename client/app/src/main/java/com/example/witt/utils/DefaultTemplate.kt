05d42add8590f7f608808ce5dcd11b443c7317ee
jaino <msb3302@naver.com>
2022-11-23 10:20:53 +0900

    issue #88 chore : planName -> tripName, date -> tripDate 변경

--- client/app/src/main/java/com/example/witt/utils/DefaultTemplate.kt
+++ client/app/src/main/java/com/example/witt/utils/DefaultTemplate.kt
@@ -8,9 +8,9 @@ object DefaultTemplate {
     fun createTemplate(plan: PlanStateModel): FeedTemplate{
         return FeedTemplate(
             content = Content(
-                title = "${plan.Name} 여행에 초대합니다!",
+                title = "'${plan.Name}' 여행에 초대합니다!",
                 description = "일정을 함께 작성해보세요.",
-                imageUrl = "https://github.com/pknu-wap/2022_2_WAP_APP_TEAM1/blob/develop_android/image/icon.png?raw=true",
+                imageUrl = "https://github.com/pknu-wap/2022_2_WAP_A.PP_TEAM1/blob/develop_android/image/icon.png?raw=true",
                 link = Link(
                     webUrl = "https://developers.kakao.com",
                     mobileWebUrl = "https://developers.kakao.com"
@@ -22,8 +22,8 @@ object DefaultTemplate {
                     Link(
                         androidExecutionParams = mapOf(
                             "tripId" to plan.TripId.toString(),
-                            "planName" to plan.Name,
-                            "date" to plan.StartDate + '~' + plan.EndDate
+                            "tripName" to plan.Name,
+                            "tripDate" to plan.StartDate + '~' + plan.EndDate
                         )
                     )
                 )
