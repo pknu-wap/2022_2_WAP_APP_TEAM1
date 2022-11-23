714def4969d20966084778fa20662445c356403a
jaino <msb3302@naver.com>
2022-11-23 09:39:15 +0900

    issue #88 feature : 카카오 링크 템플릿 image, 및 데이터 변경

--- client/app/src/main/java/com/example/witt/utils/DefaultTemplate.kt
+++ client/app/src/main/java/com/example/witt/utils/DefaultTemplate.kt
@@ -1,14 +1,16 @@
 package com.example.witt.utils
 
+import com.example.witt.domain.model.plan.PlanStateModel
 import com.kakao.sdk.template.model.*
 
 object DefaultTemplate {
-    val kakaoTemplate by lazy{
-        FeedTemplate(
+
+    fun createTemplate(plan: PlanStateModel): FeedTemplate{
+        return FeedTemplate(
             content = Content(
-                title = "WitT의 여행에 초대합니다!",
+                title = "${plan.Name} 여행에 초대합니다!",
                 description = "일정을 함께 작성해보세요.",
-                imageUrl = "https://avatars.githubusercontent.com/u/77484719?v=4",
+                imageUrl = "https://github.com/pknu-wap/2022_2_WAP_APP_TEAM1/blob/develop_android/image/icon.png?raw=true",
                 link = Link(
                     webUrl = "https://developers.kakao.com",
                     mobileWebUrl = "https://developers.kakao.com"
@@ -18,7 +20,11 @@ object DefaultTemplate {
                 Button(
                     "초대 수락하기",
                     Link(
-                        androidExecutionParams = mapOf("key1" to "value1")
+                        androidExecutionParams = mapOf(
+                            "tripId" to plan.TripId.toString(),
+                            "planName" to plan.Name,
+                            "date" to plan.StartDate + '~' + plan.EndDate
+                        )
                     )
                 )
             )
