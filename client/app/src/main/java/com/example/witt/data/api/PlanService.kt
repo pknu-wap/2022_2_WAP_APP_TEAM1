c34356470cb7cd73ceec68d252768893fcccb0fa
jaino <msb3302@naver.com>
2022-11-24 15:20:06 +0900

    issue #88 feature : outPlan Api interface 작성

--- client/app/src/main/java/com/example/witt/data/api/PlanService.kt
+++ client/app/src/main/java/com/example/witt/data/api/PlanService.kt
@@ -1,12 +1,14 @@
 package com.example.witt.data.api
 
+import com.example.witt.data.model.plan.join_plan.JoinPlanResponse
+import com.example.witt.data.model.plan.join_plan.OutPlanResponse
 import com.example.witt.data.model.plan.make_plan.request.MakePlanRequest
 import com.example.witt.data.model.plan.make_plan.response.MakePlanResponse
-import com.example.witt.data.model.plan.remove_plan.JoinPlanResponse
 import com.example.witt.data.model.plan.remove_plan.RemovePlanResponse
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface PlanService {
 
@@ -20,9 +22,14 @@ interface PlanService {
         @Body tripId: Int
     ): RemovePlanResponse
 
-    @PUT("/api/trip")
+    @PUT("/api/trip/{tripId}/participants")
     suspend fun joinPlan(
-        @Body tripId: Int
+        @Path ("tripId") tripId: Int
     ): JoinPlanResponse
+
+    @DELETE("/api/trip/{tripId}/participants")
+    suspend fun outPlan(
+        @Path ("tripId") tripId: Int
+    ): OutPlanResponse
 
 }
