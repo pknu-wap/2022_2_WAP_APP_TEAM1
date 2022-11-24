77b310123d162b4880cbb8c1c95b3bbaa5b1b4e1
vrexpert <vrexpert2302@gmail.com>
2022-11-25 00:43:27 +0900

    issue #93 feat(AirlineSearchFragment): 항공권 ui 틀 작성

---
+++ client/app/src/main/java/com/example/witt/presentation/ui/search/airline/AirlineSearchFragment.kt
@@ -0,0 +1,36 @@
+package com.example.witt.presentation.ui.search.airline
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.util.Pair
+import com.example.witt.R
+import com.example.witt.databinding.FragmentAirlineSearchBinding
+import com.example.witt.presentation.base.BaseFragment
+import com.google.android.material.datepicker.MaterialDatePicker
+
+class AirlineSearchFragment: BaseFragment<FragmentAirlineSearchBinding>(R.layout.fragment_airline_search){
+
+    private val dateRangePicker by lazy {
+        MaterialDatePicker.Builder.dateRangePicker()
+            .setTheme(R.style.CustomThemeOverlay_MaterialCalendar_Fullscreen)
+            .setTitleText("언제 여행을 떠나세요?")
+            .setSelection(
+                Pair(
+                    MaterialDatePicker.thisMonthInUtcMilliseconds(),
+                    MaterialDatePicker.todayInUtcMilliseconds()
+                )
+            )
+            .build()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initButton()
+    }
+
+    private fun initButton(){
+        binding.goToMakePlanButton.setOnClickListener {
+            dateRangePicker.show(requireActivity().supportFragmentManager, "datePicker")
+        }
+    }
+}
