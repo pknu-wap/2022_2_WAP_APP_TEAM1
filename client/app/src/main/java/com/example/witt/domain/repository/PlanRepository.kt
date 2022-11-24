188d1ef90bd82c77ac00d0a3140ee4c1770e4320
jaino <msb3302@naver.com>
2022-11-24 15:21:31 +0900

    issue #88 feature : create outPlan DataSource

--- client/app/src/main/java/com/example/witt/domain/repository/PlanRepository.kt
+++ client/app/src/main/java/com/example/witt/domain/repository/PlanRepository.kt
@@ -4,7 +4,8 @@ import com.example.witt.domain.model.plan.get_plan.GetPlanListModel
 import com.example.witt.domain.model.plan.get_plan.GetPlanModel
 import com.example.witt.domain.model.plan.make_plan.MakePlanModel
 import com.example.witt.domain.model.plan.make_plan.MakePlanResponseModel
-import com.example.witt.domain.model.plan.remove_plan.JoinPlanModel
+import com.example.witt.domain.model.plan.join_plan.JoinPlanModel
+import com.example.witt.domain.model.plan.join_plan.OutPlanModel
 import com.example.witt.domain.model.plan.remove_plan.RemovePlanModel
 
 interface PlanRepository {
@@ -18,5 +19,7 @@ interface PlanRepository {
     suspend fun removePlan(tripId: Int): Result<RemovePlanModel>
 
     suspend fun joinPlan(tripId: Int): Result<JoinPlanModel>
+
+    suspend fun outPlan(tripId: Int): Result<OutPlanModel>
 
 }
