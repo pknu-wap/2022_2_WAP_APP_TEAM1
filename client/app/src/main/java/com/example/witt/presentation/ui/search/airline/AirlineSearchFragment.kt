83f4d7808052abc0306dab142724d9731ce39da0
vrexpert <vrexpert2302@gmail.com>
2022-11-26 00:33:02 +0900

    issue #93 feat(AirlineSearchFragment): DatePicker 로 변경 및 날짜 formatting
    dateRangePicker 에서 datePicker 로 변경
    날짜를 'yyyy mm dd' 로 formatting

--- client/app/src/main/java/com/example/witt/presentation/ui/search/airline/AirlineSearchFragment.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/search/airline/AirlineSearchFragment.kt
@@ -2,38 +2,33 @@ package com.example.witt.presentation.ui.search.airline
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.util.Pair
 import com.example.witt.R
 import com.example.witt.databinding.FragmentAirlineSearchBinding
 import com.example.witt.presentation.base.BaseFragment
 import com.google.android.material.datepicker.MaterialDatePicker
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AirlineSearchFragment: BaseFragment<FragmentAirlineSearchBinding>(R.layout.fragment_airline_search){
 
-
-    private val dateRangePicker by lazy {
-        MaterialDatePicker.Builder.dateRangePicker()
+    private val datePicker by lazy {
+        MaterialDatePicker.Builder.datePicker()
             .setTheme(R.style.CustomThemeOverlay_MaterialCalendar_Fullscreen)
-            .setTitleText("출발하는 날짜를 선택해주세요!")
-            .setSelection(
-                Pair(
-                    MaterialDatePicker.thisMonthInUtcMilliseconds(),
-                    MaterialDatePicker.todayInUtcMilliseconds()
-                )
-            )
+            .setTitleText("날짜를 선택해주세요!")
             .build()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initButton()
+        initDatePicker()
     }
 
     private fun initButton(){
         binding.datePeriod.setOnClickListener {
-            dateRangePicker.show(requireActivity().supportFragmentManager, "datePicker")
+            datePicker.show(requireActivity().supportFragmentManager, "datePicker")
         }
         binding.airportStart.setOnClickListener{
             showDialog(it as TextView)
@@ -42,6 +37,19 @@ class AirlineSearchFragment: BaseFragment<FragmentAirlineSearchBinding>(R.layout
             showDialog(it as TextView)
         }
     }
+
+    private fun initDatePicker() {
+        datePicker.addOnPositiveButtonClickListener {
+            binding.datePeriod.text= formattingDate(it)
+        }
+    }
+
+    private fun formattingDate(time:Long):String{
+        val date = Date(time)
+        val format = SimpleDateFormat("yyyy.MM.dd")
+        return format.format(date)
+    }
+
     private fun showDialog(it: TextView){
         val airports = resources.getStringArray(R.array.airport_array)
         val builder = AlertDialog.Builder(requireActivity())
@@ -52,7 +60,5 @@ class AirlineSearchFragment: BaseFragment<FragmentAirlineSearchBinding>(R.layout
             }
         builder.show()
     }
-
-
 
 }
