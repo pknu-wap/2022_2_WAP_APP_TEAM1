1a18d5bb5196f3d3a68ab3364cd761a6b290d6e5
jaino <msb3302@naver.com>
2022-11-29 16:21:21 +0900

    issue #96 feature : add Place Repository 구현

--- client/app/src/main/java/com/example/witt/domain/repository/DetailPlanRepository.kt
+++ client/app/src/main/java/com/example/witt/domain/repository/DetailPlanRepository.kt
@@ -1,5 +1,7 @@
 package com.example.witt.domain.repository
 
+import com.example.witt.domain.model.plan.detail_plan.AddPlaceModel
+import com.example.witt.domain.model.plan.detail_plan.AddPlaceResponseModel
 import com.example.witt.domain.model.plan.memo.EditMemoModel
 import com.example.witt.domain.model.plan.memo.MakeMemoModel
 
@@ -8,5 +10,7 @@ interface DetailPlanRepository {
     suspend fun makeMemo(tripId : Int, day: Int, content: String) : Result<MakeMemoModel>
 
     suspend fun editMemo(tripId: Int, planId: Int, Content: String): Result<EditMemoModel>
+
+    suspend fun addPlace(tripId: Int, day: Int, content: AddPlaceModel): Result<AddPlaceResponseModel>
 
 }
