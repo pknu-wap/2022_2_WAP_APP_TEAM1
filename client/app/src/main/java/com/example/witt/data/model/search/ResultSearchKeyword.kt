207f26493f87a36f49764b684bc02ba0f13e5037
jaino <msb3302@naver.com>
2022-11-29 16:23:30 +0900

    issue #96 feature : 검색 결과 DTO 카테고리 추가 및 AddPlaceModel Mapper 구현

--- client/app/src/main/java/com/example/witt/data/model/search/ResultSearchKeyword.kt
+++ client/app/src/main/java/com/example/witt/data/model/search/ResultSearchKeyword.kt
@@ -1,6 +1,7 @@
 package com.example.witt.data.model.search
 
 import android.os.Parcelable
+import com.example.witt.domain.model.plan.detail_plan.AddPlaceModel
 import kotlinx.parcelize.Parcelize
 
 
@@ -13,8 +14,8 @@ data class ResultSearchKeyword(
 data class PlaceModel(
     var id: String,                     // 장소 ID
     var place_name: String,             // 장소명, 업체명
-//  var category_name: String,          // 카테고리 이름
-//  var category_group_code: String,    // 중요 카테고리만 그룹핑한 카테고리 그룹 코드
+    var category_name: String,          // 카테고리 이름
+//    var category_group_code: String,    // 중요 카테고리만 그룹핑한 카테고리 그룹 코드
     var category_group_name: String,    // 중요 카테고리만 그룹핑한 카테고리 그룹명
 //  var phone: String,                  // 전화번호
     var address_name: String,           // 전체 지번 주소
@@ -24,3 +25,12 @@ data class PlaceModel(
 //    var place_url: String,              // 장소 상세페이지 URL
 //    var distanc: String                 // 중심좌표까지의 거리. 단, x,y 파라미터를 준 경우에만 존재. 단위는 meter
 ) : Parcelable
+
+fun PlaceModel.toAddPlaceRequest() = AddPlaceModel(
+    placeId = id,
+    latitude = y,
+    longitude = x,
+    category = category_name,
+    administrationCode = id.toInt(),
+    name = place_name
+)
