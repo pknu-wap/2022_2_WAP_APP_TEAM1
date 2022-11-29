74d62c706b36bccbca395d11d1ff86ad76954650
jaino <msb3302@naver.com>
2022-11-29 21:10:12 +0900

    issue #96 chore : outPlan url edit

--- client/app/src/main/java/com/example/witt/data/api/PlanService.kt
+++ client/app/src/main/java/com/example/witt/data/api/PlanService.kt
@@ -4,7 +4,6 @@ import com.example.witt.data.model.plan.join_plan.JoinPlanResponse
 import com.example.witt.data.model.plan.join_plan.OutPlanResponse
 import com.example.witt.data.model.plan.make_plan.request.MakePlanRequest
 import com.example.witt.data.model.plan.make_plan.response.MakePlanResponse
-import com.example.witt.data.model.plan.remove_plan.RemovePlanResponse
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.PUT
@@ -17,14 +16,14 @@ interface PlanService {
         @Body request: MakePlanRequest
     ):MakePlanResponse
 
+    @DELETE("/api/trip/{tripId}")
+    suspend fun outPlan(
+        @Path ("tripId") tripId: Int
+    ): OutPlanResponse
+
     @PUT("/api/trip/{tripId}/participants")
     suspend fun joinPlan(
         @Path ("tripId") tripId: Int
     ): JoinPlanResponse
-
-    @DELETE("/api/trip/{tripId}/participants")
-    suspend fun outPlan(
-        @Path ("tripId") tripId: Int
-    ): OutPlanResponse
 
 }
