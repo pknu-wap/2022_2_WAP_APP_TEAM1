fccaf6773852f266bbf30d539f325184a35b5904
vrexpert <vrexpert2302@gmail.com>
2022-11-30 10:12:40 +0900

    issue #93 feat(FlightSearchFragment): FlightSearchFragment 생성 및 로직 구현
    planViewModel.planState 를 observe 하여 그 기간을 tripDateRange 에 저장
    항공사 선택 dialog 초기화
    Date 선택 dialog 초기화

---
+++ client/app/src/main/java/com/example/witt/presentation/ui/search/airline/FlightSearchFragment.kt
@@ -0,0 +1,81 @@
+package com.example.witt.presentation.ui.search.airline
+
+import android.os.Bundle
+import android.view.View
+import com.example.witt.R
+import com.example.witt.presentation.base.BaseFragment
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.activityViewModels
+import com.example.witt.databinding.FragmentFlightSearchBinding
+import com.example.witt.presentation.ui.plan.PlanViewModel
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class FlightSearchFragment: BaseFragment<FragmentFlightSearchBinding>(R.layout.fragment_flight_search){
+
+    private val planViewModel by activityViewModels<PlanViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAirline()
+        observeData()
+    }
+
+    private fun observeData() {
+        planViewModel.planState.observe(viewLifecycleOwner) {
+            it.TripId
+            val sd = LocalDate.parse(it.StartDate, DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+            val ed = LocalDate.parse(it.EndDate, DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+            initDate(tripDateRange(sd,ed))
+        }
+    }
+
+    //Date 리스트 생성
+    private fun tripDateRange(sd:LocalDate,ed:LocalDate):List<LocalDate>{
+        var dateRange = mutableListOf<LocalDate>()
+        var date = sd
+        while(date<=ed){
+            dateRange.add(date)
+            date = date.plusDays(1)
+        }
+        return dateRange
+    }
+
+    //항공사 선택 dialog 초기화
+    private fun initAirline(){
+        binding.airline.setOnClickListener{
+            showAirlineDialog(it as TextView)
+        }
+    }
+
+    private fun showAirlineDialog(it: TextView) {
+        val airports = resources.getStringArray(R.array.airline_array)
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setTitle("공항을 선택하세요.")
+            .setItems(
+                airports
+            ) { _, which ->
+                it.text = airports[which]
+            }
+        builder.show()
+    }
+
+
+    //Date 선택 dialog 초기화
+    private fun initDate(dateRange:List<LocalDate>){
+        binding.date.setOnClickListener {
+            showDateDialog(it as TextView,dateRange)
+        }
+    }
+
+    private fun showDateDialog(it: TextView, dateRange: List<LocalDate>){
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setTitle("날짜를 선택하세요.")
+            .setItems(dateRange.size){
+                _,which ->
+                it.text = dateRange[which].toString()
+            }
+        builder.show()
+    }
+}
