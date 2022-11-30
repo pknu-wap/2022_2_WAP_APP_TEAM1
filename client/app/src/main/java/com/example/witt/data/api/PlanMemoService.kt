a6d173254dc150e3484292c816bb00645f1f33e2
jaino <msb3302@naver.com>
2022-11-30 21:24:57 +0900

    issue #96 feature : planMemoService FormUrlEncoded to Json

--- client/app/src/main/java/com/example/witt/data/api/PlanMemoService.kt
+++ client/app/src/main/java/com/example/witt/data/api/PlanMemoService.kt
@@ -1,25 +1,25 @@
 package com.example.witt.data.api
 
+import com.example.witt.data.model.plan.detail_plan.request.MemoRequest
 import com.example.witt.data.model.plan.memo.EditMemoResponse
 import com.example.witt.data.model.plan.memo.MakeMemoResponse
 import retrofit2.http.*
 
 interface PlanMemoService {
 
-    @FormUrlEncoded
+
     @PUT("/api/trip/{tripId}/plan/days/{day}/memo")
     suspend fun makeMemo(
         @Path("tripId") planId: Int,
         @Path("day") day: Int,
-        @Field("Content") Content: String
+        @Body Content: MemoRequest
     ) : MakeMemoResponse
 
-    @FormUrlEncoded
     @PATCH("/api/trip/{tripId}/plan/{planId}/memo")
     suspend fun editMemo(
         @Path("tripId") tripId: Int,
         @Path("planId") planId: Int,
-        @Field("Content") Content: String
+        @Body Content: MemoRequest
     ): EditMemoResponse
 
 }
