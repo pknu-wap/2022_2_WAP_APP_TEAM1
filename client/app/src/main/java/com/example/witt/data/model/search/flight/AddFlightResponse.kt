0cf98fef826687d5449f1ae3d011295b34191b02
vrexpert <vrexpert2302@gmail.com>
2022-12-02 04:39:34 +0900

    issue #93 feat: 항공편 API 응답 데이터 클래스 생성

---
+++ client/app/src/main/java/com/example/witt/data/model/search/flight/AddFlightResponse.kt
@@ -0,0 +1,18 @@
+package com.example.witt.data.model.search.flight
+
+data class AddFlightResponse (
+    val status:Boolean,
+    val reason:String,
+    val planId:Int,
+    val planFlight:PlanFlight
+)
+
+data class PlanFlight(
+    val TripId :Int,
+    val PlanId:Int,
+    val AirlineCode:String,
+    val FlightNum:String,
+    val DepartureTime:String,
+    val ArrivalTime:String,
+    val DepartureAirport:String
+)
