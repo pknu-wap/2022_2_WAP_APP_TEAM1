7042b76fd233f91198f5dac47297d9718350274e
vrexpert <vrexpert2302@gmail.com>
2022-12-01 16:56:14 +0900

    issue #93 feat: SearchFlightModel 추가

---
+++ client/app/src/main/java/com/example/witt/domain/model/flight/SearchFlightModel.kt
@@ -0,0 +1,19 @@
+package com.example.witt.domain.model.flight
+
+import com.example.witt.data.model.search.flight.Flight
+
+
+data class SearchFlightModel(
+    val Status:Boolean,
+    val Result:String,
+    val Flight: Flight
+)
+
+data class Flight(
+    val airline :String,
+    val flightNo:Int,
+    val flyFrom:String,
+    val flyTo:String,
+    val departure:String,
+    val arrival:String
+)
