9bc4b8f0d64f87bbd14bed98cbb078b2f0485fd4
jaino <msb3302@naver.com>
2022-12-01 10:15:33 +0900

    issue #102 feature : isoString to DateString converter 구현

--- client/app/src/main/java/com/example/witt/utils/TimeConverter.kt
+++ client/app/src/main/java/com/example/witt/utils/TimeConverter.kt
@@ -1,6 +1,12 @@
 package com.example.witt.utils
 
 import android.text.format.DateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun Long.convertTimestampToDate(): String =
     DateFormat.format("yyyy.MM.dd", this).toString()
+
+fun String.convertIsoToDate(): String =
+     LocalDateTime.parse(this, DateTimeFormatter.ISO_DATE_TIME)
+         .format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
