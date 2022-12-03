85967ba8cab290b2fbe5d18a1fcd05d89ac7af7b
jaino <msb3302@naver.com>
2022-12-03 12:22:38 +0900

    issue #111 chore : 이메일 중복 검사 url 수정

--- client/app/src/main/java/com/example/witt/data/api/DuplicateEmailService.kt
+++ client/app/src/main/java/com/example/witt/data/api/DuplicateEmailService.kt
@@ -1,15 +1,14 @@
 package com.example.witt.data.api
 
-import com.example.witt.data.model.user.request.DuplicateEmailRequest
 import com.example.witt.data.model.user.response.DuplicateEmailResponse
 import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface DuplicateEmailService {
 
-    @GET("/api/user/duplicate")
+    @GET("api/user/new")
     suspend fun duplicateEmailCheck(
-        @Header("Username") request: DuplicateEmailRequest
+        @Query("Username") Username: String
     ): DuplicateEmailResponse
 
 }
