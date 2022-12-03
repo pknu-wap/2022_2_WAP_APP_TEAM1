47bfbef403603d7dcec3aef38250d967be1decc4
jaino <msb3302@naver.com>
2022-12-03 13:17:36 +0900

    issue #112 rename : duplicateEmailService -> SignUpService

--- client/app/src/main/java/com/example/witt/data/api/SignUpService.kt
+++ client/app/src/main/java/com/example/witt/data/api/SignUpService.kt
@@ -2,12 +2,22 @@ package com.example.witt.data.api
 
 import com.example.witt.data.model.auth.request.SignUpRequest
 import com.example.witt.data.model.auth.response.SignUpResponse
+import com.example.witt.data.model.user.response.DuplicateEmailResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface SignUpService {
+
     @POST("/api/user/new")
     suspend fun signUp(
         @Body request: SignUpRequest
     ): SignUpResponse
+
+    @GET("api/user/new")
+    suspend fun duplicateEmailCheck(
+        @Query("Username") Username: String
+    ): DuplicateEmailResponse
+
 }
--- client/app/src/main/java/com/example/witt/data/source/remote/duplicate_check/DuplicateEmailDataSourceImpl.kt
+++ client/app/src/main/java/com/example/witt/data/source/remote/duplicate_check/DuplicateEmailDataSourceImpl.kt
@@ -1,6 +1,6 @@
 package com.example.witt.data.source.remote.duplicate_check
 
-import com.example.witt.data.api.DuplicateEmailService
+import com.example.witt.data.api.SignUpService
 import com.example.witt.data.model.user.request.DuplicateEmailRequest
 import com.example.witt.data.model.user.response.DuplicateEmailResponse
 import com.example.witt.di.IoDispatcher
@@ -9,7 +9,7 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class DuplicateEmailDataSourceImpl @Inject constructor(
-    private val duplicateEmailService: DuplicateEmailService,
+    private val duplicateEmailService: SignUpService,
     @IoDispatcher private val coroutineDispatcher: CoroutineDispatcher
 ):DuplicateEmailDataSource{
     override suspend fun duplicateEmailCheck(duplicateEmailRequest: DuplicateEmailRequest): Result<DuplicateEmailResponse>
