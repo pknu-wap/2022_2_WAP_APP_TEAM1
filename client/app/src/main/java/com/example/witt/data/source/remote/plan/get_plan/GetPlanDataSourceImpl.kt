3268dc2aa176c81a26db8d6801fc38bc12b1b012
jaino <msb3302@naver.com>
2022-12-03 13:18:15 +0900

    issue #112 rename : getPlanService -> PlanService

--- client/app/src/main/java/com/example/witt/data/source/remote/plan/get_plan/GetPlanDataSourceImpl.kt
+++ client/app/src/main/java/com/example/witt/data/source/remote/plan/get_plan/GetPlanDataSourceImpl.kt
@@ -1,6 +1,6 @@
 package com.example.witt.data.source.remote.plan.get_plan
 
-import com.example.witt.data.api.GetPlanService
+import com.example.witt.data.api.PlanService
 import com.example.witt.data.model.plan.get_plan.GetPlanResponse
 import com.example.witt.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
@@ -8,7 +8,7 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetPlanDataSourceImpl @Inject constructor(
-    private val getPlanService: GetPlanService,
+    private val getPlanService: PlanService,
     @IoDispatcher private val coroutineDispatcher: CoroutineDispatcher
 ): GetPlanDataSource {
     override suspend fun getPlan(planId: Int): Result<GetPlanResponse>
--- client/app/src/main/java/com/example/witt/data/source/remote/plan/get_plan/GetPlanListDataSourceImpl.kt
+++ client/app/src/main/java/com/example/witt/data/source/remote/plan/get_plan/GetPlanListDataSourceImpl.kt
@@ -1,6 +1,6 @@
 package com.example.witt.data.source.remote.plan.get_plan
 
-import com.example.witt.data.api.GetPlanService
+import com.example.witt.data.api.PlanService
 import com.example.witt.data.model.plan.get_plan.GetPlanListResponse
 import com.example.witt.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
@@ -8,7 +8,7 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetPlanListDataSourceImpl @Inject constructor(
-    private val getPlanService: GetPlanService,
+    private val getPlanService: PlanService,
     @IoDispatcher private val coroutineDispatcher: CoroutineDispatcher
 ): GetPlanListDataSource {
     override suspend fun getPlanList(): Result<GetPlanListResponse>
