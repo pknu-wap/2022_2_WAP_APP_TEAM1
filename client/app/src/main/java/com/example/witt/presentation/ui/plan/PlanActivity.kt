c564722f3063ef5ffc323accbb4951563c205f65
jaino <msb3302@naver.com>
2023-02-08 15:36:29 +0900

    issue #134 chore : MainActivity -> PlanActivity navigation이 아닌, intent로 화면 전환

--- client/app/src/main/java/com/example/witt/presentation/ui/plan/PlanActivity.kt
+++ client/app/src/main/java/com/example/witt/presentation/ui/plan/PlanActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.example.witt.R
 import com.example.witt.databinding.ActivityPlanBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,7 +25,6 @@ class PlanActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.plan_nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
